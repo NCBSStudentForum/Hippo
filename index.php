490159d7dbda211070c4de7d67d4d52f248e1e3b
Dilawar Singh <dilawars@ncbs.res.in>
2016-10-27 15:39:09 +0530

    Added count file.
    Former-commit-id: b180d3d6589e0314209db30bb89714e8bf81ba41
    Former-commit-id: 606094e1c032dd3082ec60dfe5e63aa5197e0660

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
 <?php
-require_once( "header.php" );
+include('header.php');
+include('methods.php');
+include('tables.php');
+
+session_save_path("/tmp/");
+$conf = array();
+$inifile = "bmvrc";
+if(file_exists($inifile)) {
+	$conf = parse_ini_file($inifile, $process_sections = true );
+}
+
+if(!$conf)
+{
+    $error = "Failed to read  configuartion file.";
+    header($error." I can't do anything anymore. Please wake up the admin.");
+    exit;
+}
+
+$_SESSION['conf'] = $conf;
+
+// Has multable tables, log, users etc.
+$_SESSION['db'] = 'db/bmv.sqlite';
+
+/* counter */
+$hit_count = (int)file_get_contents('count.txt');
+$hit_count++;
+file_put_contents('count.txt', $hit_count);
+
 ?>
 
 <html>

45838bac3a2a7621f78159d51f22b39114ee216e
Dilawar Singh <dilawars@ncbs.res.in>
2016-10-28 16:48:36 +0530

    Added missing php files.
    Former-commit-id: ee01739d1c5965a85d07723c8d660a257a55ac2a
    Former-commit-id: 95bca250bbac10958324c0f7d440e2410998bad1

---
+++ request_action.php
@@ -0,0 +1,36 @@
+<?php
+include_once( "header.php" );
+include_once( "methods.php" );
+include_once( "sqlite.php" );
+
+//var_dump( $_POST );
+
+if( $_POST[ "response" ] == "Go back" )
+{
+    goToPage( "index.php", 0 );
+    exit( 0 );
+}
+
+$repeatPat = $_POST[ 'repeatPat' ];
+$conn = connectDB( );
+
+$query = $conn->prepare( 
+    "INSERT INTO requests ( 
+        requestBy, venue, description, startOn, endOn, repeatPat, timestamp, status 
+    ) VALUES ( 
+        :requestBy, :venue, :description, :startOn, :endOn, :repeatPat, 'now', 'pending' 
+    )");
+
+$query->bindValue( ':requestBy', $_SESSION['user'] );
+$query->bindValue( ':venue' , $_POST['venueId' ] );
+$query->bindValue( ':description', $_POST['description'] );
+$query->bindValue( ':startOn', $_POST['startOn'] );
+$query->bindValue( ':endOn', $_POST['endOn'] );
+$query->bindValue( ':repeatPat', $_POST['repeatPat'] );
+$res = $query->execute();
+$conn->close();
+
+echo printInfo( "Your request has been submitted." );
+goToPage( "index.php", 3 );
+
+?>

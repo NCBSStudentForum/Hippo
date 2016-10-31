251b4d0d49a64f7a933fd43f7a6f2ee4a875475c
Dilawar Singh <dilawars@ncbs.res.in>
2016-10-31 16:59:55 +0530

    Added missing files.
    Former-commit-id: 23b61fa0429bdaed5553a88283a5b897c2fe8df1
    Former-commit-id: cde09791999688d7a8859d065871f621fb35b0f4

--- events.json.php
+++ events.json.php
@@ -1,77 +1,21 @@
+<?php 
+include_once( "database.php" );
+
+// Write the list of events in json format. This url is used by Calendar class 
+// to read the event list and populate its user-interface accordingly.
+$upcomingEvents = getEvents( strtotime( 'today' ) );
+$formattedEvents = Array( );
+foreach( $upcomingEvents as $event )
 {
-	"success": 1,
-	"result": [
-		{
-			"id": "293",
-			"title": "This is warning class event with very long title to check how it fits to evet in day view",
-			"url": "http://www.example.com/",
-			"class": "event-warning",
-			"start": "1362938400000",
-			"end":   "1363197686300"
-		},
-		{
-			"id": "256",
-			"title": "Event that ends on timeline",
-			"url": "http://www.example.com/",
-			"class": "event-warning",
-			"start": "1363155300000",
-			"end":   "1363227600000"
-		},
-		{
-			"id": "276",
-			"title": "Short day event",
-			"url": "http://www.example.com/",
-			"class": "event-success",
-			"start": "1363245600000",
-			"end":   "1363252200000"
-		},
-		{
-			"id": "294",
-			"title": "This is information class ",
-			"url": "http://www.example.com/",
-			"class": "event-info",
-			"start": "1363111200000",
-			"end":   "1363284086400"
-		},
-		{
-			"id": "297",
-			"title": "This is success event",
-			"url": "http://www.example.com/",
-			"class": "event-success",
-			"start": "1363234500000",
-			"end":   "1363284062400"
-		},
-		{
-			"id": "54",
-			"title": "This is simple event",
-			"url": "http://www.example.com/",
-			"class": "",
-			"start": "1363712400000",
-			"end":   "1363716086400"
-		},
-		{
-			"id": "532",
-			"title": "This is inverse event",
-			"url": "http://www.example.com/",
-			"class": "event-inverse",
-			"start": "1364407200000",
-			"end":   "1364493686400"
-		},
-		{
-			"id": "548",
-			"title": "This is special event",
-			"url": "http://www.example.com/",
-			"class": "event-special",
-			"start": "1363197600000",
-			"end":   "1363629686400"
-		},
-		{
-			"id": "295",
-			"title": "Event 3",
-			"url": "http://www.example.com/",
-			"class": "event-important",
-			"start": "1364320800000",
-			"end":   "1364407286400"
-		}
-	]
+    $e = Array();
+    $e['id'] = $event['gid'] . '.' . $event['eid'];
+    $e['class'] = ''; //$event['class'];
+    $e['title'] = $event['short_description'];
+    $e['url'] = ''; //$event['url'];
+    $e['start'] = strtotime( $event['date'] . ' ' . $event['start_time']);
+    $e['end'] = strtotime( $event['date'] . ' ' . $event['end_time']);
+    array_push( $formattedEvents, $e );
 }
+
+echo ( '{ "success" : 1, "result":' . json_encode( $formattedEvents ) . "}");
+?>

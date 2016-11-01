1b8c84608525809e33bbcc41633bdef89d48c188
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-01 11:51:44 +0530

    Select ranges of date and populate the table accordingly.

--- user.php
+++ user.php
@@ -14,20 +14,41 @@ $venueSelect = venuesToHTMLSelect( $venues, true );
 // We came to this page without default option. Let's fill them in $_POST. We 
 // are going to iterate over this page for its mandatory to create $_POST.
 if( ! array_key_exists( 'date', $_POST ) )
+{
     $_POST['date'] = humanReadableDate( strtotime( 'today' ) );
+    $_POST['end_date'] = humanReadableDate( strtotime( 'today' ) );
+}
 
 // If no venue if selected then use all venues.
 if( ! array_key_exists( 'venue', $_POST ) )
     $_POST['venue'] = array_map( function($a) { return $a['id']; }, $venues);
 
 echo "<form method=\"post\" action=\"user.php\">
-    <input type=\"date\" name=\"date\" placeholder=\"Select date\" >
-    $venueSelect
-    <button name=\"response\" value=\"submit\">Submit</button>
-    </form>";
+    <table>
+    <tr>
+        <th>Start date</th><th>End date</th><th>Select Venues<th><th> </th>
+    </tr>
+    <tr>
+    <td><input type=\"date\" name=\"date\" placeholder=\"Select start date\" ></td>
+    <td><input type=\"date\" name=\"end_date\" placeholder=\"Select end date\" ></td>
+    <td>  $venueSelect </td>
+    <td>
+    <button style=\"float:right\" name=\"response\" value=\"submit\">Submit</button>
+    </td>
+    </tr>
+    </table>
+    </form>
+    <br><br>
+    ";
 
 
 $date = $_POST['date'];
+$endData = $_POST['end_date'];
+if( ! $endDate )
+    $endDate = $date;
+
+echo "Start Date: $date to $endDate <br>";
+
 $day = nameOfTheDay( $date );
 
 $html = "<h3>On $day, $date </h3>";
@@ -43,6 +64,8 @@ echo "<table>
     </td></tr>
     </table>";
 echo "</div>";
+
+// Now generate the range of dates.
 
 // Now generate eventline for each venue.
 foreach( $_POST['venue'] as $venueid )

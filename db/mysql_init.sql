540a831730e7f84fb97a0e051673d2796c64df97
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-01 14:56:50 +0530

    Initialized a test database.
    Former-commit-id: f9a1ef6941f930d412c8bd563caa968d097a23ab
    Former-commit-id: b65c571104278b449b3906609a71ce98b9e3eb78

--- db/mysql_init.sql
+++ db/mysql_init.sql
@@ -3,7 +3,8 @@ USE bookmyvenue_test;
 
 DROP TABLE IF EXISTS requests;
 CREATE TABLE IF NOT EXISTS requests (
-    gid INT NOT NULL, rid INT NOT NULL
+    gid INT NOT NULL
+    , rid INT NOT NULL
     , user VARCHAR(50) NOT NULL
     , title VARCHAR(100) NOT NULL
     , description TEXT 
@@ -15,15 +16,36 @@ CREATE TABLE IF NOT EXISTS requests (
     , timestamp TIMESTAMP  DEFAULT CURRENT_TIMESTAMP 
     , PRIMARY KEY( gid, rid )
     );
+
+-- venues must created before events because events refer to venues key as
+-- foreign key.
+DROP TABLE IF EXISTS venues;
+CREATE TABLE IF NOT EXISTS venues (
+    id VARCHAR(80) NOT NULL
+    , name VARCHAR(300) NOT NULL
+    , location VARCHAR(500) NOT NULL
+    , institute ENUM( 'NCBS', 'CCAMP', 'INSTEM', 'OTHER' ) DEFAULT 'NCBS'
+    , type ENUM ( 'AUDITORIUM', 'LECTURE HALL', 'MEETING ROOM'
+        , 'SPORTS', 'RECREATION', 'OPEN AIR', 'CAFETERIA'
+        , 'CENTER' ) NOT NULL
+    , strength INT NOT NULL
+    , distance_from_ncbs DECIMAL(3,3) DEFAULT 0.0 
+    , has_projector ENUM( 'YES', 'NO' ) NOT NULL
+    , suitable_for_conference ENUM( 'YES', 'NO' ) NOT NULL
+    , PRIMARY KEY (id)
+    );
+    
     
 DROP TABLE IF EXISTS events;
 CREATE TABLE IF NOT EXISTS events (
     -- Sub even will be parent.children format.
-    gid INT NOT NULL, eid INT NOT NULL
-    , type ENUM( 'PUBLIC', 'PRIVATE' ) DEFAULT 'PRIVATE' 
+    gid INT NOT NULL
+    , eid INT NOT NULL
+    , for_public_calendar ENUM( 'YES', 'NO' ) DEFAULT 'NO' 
     , class ENUM( 
-        'LABMEET', 'LECTURE', 'MEETING'
-        , 'CONFERENCE', 'UNKNOWN', 'CULTURAL'
+        'LABMEET', 'LECTURE', 'MEETING', 'SEMINAR', 'TALK'
+        , 'CONFERENCE', 'CULTURAL', 'AWS'
+        , 'UNKNOWN'
         ) DEFAULT 'UNKNOWN' 
     , short_description VARCHAR(200) NOT NULL
     , description TEXT
@@ -34,20 +56,5 @@ CREATE TABLE IF NOT EXISTS events (
     , end_time TIME NOT NULL
     , PRIMARY KEY( gid, eid )
     , FOREIGN KEY (venue) REFERENCES venues(id)
-    );
-    
-DROP TABLE IF EXISTS venues;
-CREATE TABLE IF NOT EXISTS venues (
-    id VARCHAR(80) PRIMARY KEY
-    , name VARCHAR(300) NOT NULL
-    , location VARCHAR(500) NOT NULL
-    , institute ENUM( 'NCBS', 'CCAMP', 'INSTEM', 'OTHER' )
-    , type ENUM ( 'AUDITORIUM', 'LECTURE HALL', 'MEETING ROOM'
-        , 'SPORTS', 'RECREATION', 'OPEN AIR', 'CAFETERIA'
-        , 'CENTER' ) NOT NULL; 
-    , strength INT NOT NULL
-    , distance_from_ncbs DECIMAL(3,3) DEFAULT 0.0 
-    , has_projector ENUM( 'YES', 'NO' ) NOT NULL
-    , suitable_for_conference ENUM( 'YES', 'NO' ) NOT NULL
     );
     

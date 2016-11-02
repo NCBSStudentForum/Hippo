573a22f02f344938a5f18b949906796f0674f40c
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-02 18:34:54 +0530

    Now show events.
    Former-commit-id: c52fd83dd09e27aca45641ed37b19dbd2822ed75
    Former-commit-id: 140030a9949badeb1e3771f5905e31cdc7bc3955

---
+++ user_show_events.php
@@ -0,0 +1,37 @@
+<?php
+
+include_once( "header.php" );
+include_once( "methods.php" );
+include_once( "database.php" );
+include_once( "tohtml.php" );
+
+echo userHTML( );
+
+?>
+
+<?php
+
+$requests = getRequestOfUsers( $_SESSION['user'], $status = 'PENDING' );
+
+foreach( $requests as $request )
+{
+    $gid = $request['gid'];
+    echo "<table class=\"request_edit\" >";
+    echo "<tr>";
+    echo "<td>" . arrayToTableHTML( $request, "request" );
+    echo '<form method="post" action="user_show_requests_edit.php">';
+    echo "</td><td><button name=\"response\" value=\"edit\">Edit</button>";
+    echo "</td><td><button name=\"response\" value=\"delete\">Delete</button>";
+    echo "</td></tr>";
+    echo "</table>";
+    echo "<input type=\"hidden\" name=\"gid\" value=\"$gid\">";
+    echo '</form>';
+}
+
+
+echo '<br>';
+echo '<div style="float:left">';
+echo goBackToPageLink( "user.php", "Go back" );
+echo '</div>';
+
+?>

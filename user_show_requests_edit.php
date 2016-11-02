388663e2a7c2a9164d7e45ef964529d4a4ecb6de
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-02 14:29:24 +0530

    Updated html and php files. Needs to write submit changes and delete changes script.
    Former-commit-id: 7c358e5550d64b8727020dc1405c133cf28a10e6
    Former-commit-id: 4ad033fec982c68c13760842a2f17b43e1385780

---
+++ user_show_requests_edit.php
@@ -0,0 +1,41 @@
+<?php 
+
+include_once( "header.php" );
+include_once( "methods.php" );
+include_once( "database.php" );
+include_once( "tohtml.php" );
+
+//var_dump( $_POST );
+
+$gid = $_POST['gid'];
+
+$editable = Array( "title", "description" );
+
+echo "<p class=\"info\"> You can only change fields: " . implode( ", ", $editable ) 
+    . " here. If you want to change some other fields, you have to delete 
+    this request a create a new one. </p>";
+
+if( strtolower($_POST['response']) == 'edit' )
+{
+    $requests = getRequestByGroupId( $gid );
+    // We only edit once request and all other in the same group should get 
+    // modified accordingly.
+    $request = $requests[0];
+    echo "<form method=\"post\" action=\"user_show_requests_edit_submit.php\">";
+    echo requestToEditableTableHTML( $request, $editable );
+    echo "<button class=\"submit\" name=\"response\" value=\"submit\">Submit</button>";
+    echo "</form>";
+}
+
+else if( strtolower($_POST['response']) == 'delete' )
+{
+    echo "Delete";
+}
+else
+{
+    echo printWarning( "Bad response " .  $_POST['response']  );
+}
+
+echo "<div style=\"float:left\">";
+echo goBackToPageLink( "user_show_requests.php", "Go back");
+echo "</div>";

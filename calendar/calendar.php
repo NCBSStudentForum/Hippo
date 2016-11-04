3c05a114a6dcb5c078275b2466d61437c19b1717
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-04 20:26:02 +0530

    move it out of calendar which requires connection to database. Index must not have any dependency on database. It can if ini file is parsed in database as well.
    Former-commit-id: 2bcc784eee80426a7ef3b4157d5cad921b037af9
    Former-commit-id: 7bda2d138eb6dae4ae41efb8809ae966dec9335f

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -4,12 +4,6 @@ include_once( 'database.php' );
 include_once( 'methods.php' );
 include_once( 'tohtml.php' );
 
-function embdedCalendar( )
-{
-    $html = '
-        <iframe src="https://calendar.google.com/calendar/embed?src=6bvpnrto763c0d53shp4sr5rmk%40group.calendar.google.com&ctz=Asia/Calcutta" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>';
-    return $html;
-}
 
 function addEventToGoogleCalendar($calendar_name, $event )
 {

1ee54b07cb737a7eb716559891039e5fb9fdaf4a
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-06 16:45:38 +0530

    Launch for testing.

--- bookmyvenue_admin.php
+++ bookmyvenue_admin.php
@@ -6,7 +6,7 @@ include_once( "header.php" );
 include_once( "methods.php" );
 include_once( "database.php" );
 include_once( "tohtml.php" );
-include_once( "validate_privileges.php" );
+include_once( "check_access_permissions.php" );
 
 echo userHTML( );
 

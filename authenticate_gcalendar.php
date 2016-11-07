31c52cee4107f6f37b9ef9be1317e3470409269e
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-07 21:55:51 +0530

    Now I can access my dummy calendar.
    Former-commit-id: f96d735a0227720f47fce8ae4cbef4e2ca5beda4
    Former-commit-id: b811e155ca721bd3604c610d2fea36958d1a21c3

--- authenticate_gcalendar.php
+++ authenticate_gcalendar.php
@@ -1,56 +1,14 @@
 <?php 
 
-require_once './vendor/autoload.php';
-require_once './template/google-api/base.php';
+include_once 'calendar/NCBSCalendar.php';
 
-if( isset($_SESSION['calendar_client'] ) )
-    exit( 0 );
+$calendar = new NCBSCalendar( './oauth-credentials.json' );
 
-$client = new Google_Client();
+// Now move the browser to $authUrl 
+var_dump( $calendar );
 
+header( 'Location: ' . $calendar->redirectURL, False );
 
-// Authenticate the client now.
-if (!$oauth_credentials = getOAuthCredentialsFile()) {
-    echo missingOAuth2CredentialsWarning();
-    return;
-}
-
-$client->setAuthConfig($oauth_credentials);
-//$redirectURI = 'http://ghevar.ncbs.res.in/minion/admin.php';
-//$client->setRedirectURI( $redirectURI );
-$client->setScopes(
-    'https://www.googleapis.com/auth/calendar'
-);
-
-if (isset($_REQUEST['logout'])) {
-    unset($_SESSION['id_token_token']);
-}
-if (isset($_GET['code'])) {
-    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-    $client->setAccessToken($token);
-    // store in the session also
-    $_SESSION['id_token_token'] = $token;
-}
-if (
-    !empty($_SESSION['id_token_token'])
-    && isset($_SESSION['id_token_token']['id_token'])
-) {
-    $client->setAccessToken($_SESSION['id_token_token']);
-} else {
-    echo "I am here";
-    $authUrl = $client->createAuthUrl();
-    header( 'Location: ' . $authUrl, False );
-    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-    $client->setAccessToken($token);
-    // store in the session also
-    $_SESSION['id_token_token'] = $token;
-}
-
-if ($client->getAccessToken())
-    $token_data = $client->verifyIdToken();
-
-$_SESSION[ 'calendar_client'] = $client;
-exit( 0 );
-
+exit;
 
 ?>

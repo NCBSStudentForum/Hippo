491f9577da01b18a458effa3844bffa69e0c0c24
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-07 11:37:20 +0530

    Run on ghevar.

--- admin_modify_user_privileges.php
+++ admin_modify_user_privileges.php
@@ -5,6 +5,7 @@ include_once( 'check_access_permissions.php' );
 include_once( 'tohtml.php' );
 
 mustHaveAnyOfTheseRoles( Array( 'ADMIN' ) );
+echo userHTML( );
 
 if( ! $_POST['login'] )
 {
@@ -18,5 +19,7 @@ $default = getUserInfo( $_POST['login'] );
 echo '<form method="post" action="admin_modify_user_privileges_submit.php">';
 echo dbTableToHTMLTable( 'logins', $default, $editables = Array( 'roles', 'title' ) );
 echo '</form>';
+
+echo goBackToPageLink( 'admin.php', 'Go back' );
 
 ?>

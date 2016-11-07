be1e16fdf58bdda0704fbde578b2a2d50f8e909b
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-07 09:51:43 +0530

    Shell script to initialized database.

---
+++ db/initialize_everything.sh
@@ -0,0 +1,26 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: initialize_everything.sh
+# 
+#         USAGE: ./initialize_everything.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Dilawar Singh (), dilawars@ncbs.res.in
+#  ORGANIZATION: NCBS Bangalore
+#       CREATED: 11/07/2016 09:50:06 AM
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+echo "Populating database"
+read -p "Mysql user minion password " pass
+mysql -h localhost -u minion -p$pass < ./mysql_init.sql 
+mysql -h localhost -u minion -p$pass < ./venues.sql 
+

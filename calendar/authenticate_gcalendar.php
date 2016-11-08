bde2e8c7f8cb99392cc20fddefc4a8df4e9d9512
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-08 22:39:45 +0530

    I can update individual events.
    Former-commit-id: e0dccf2271018c1fc67113bbf3ec622ed1fd359a
    Former-commit-id: da6a67e971ebd07087fc3ef289b76e1039bb8dd2

--- calendar/authenticate_gcalendar.php
+++ calendar/authenticate_gcalendar.php
@@ -1,9 +1,9 @@
 <?php 
 
-include_once 'calendar/NCBSCalendar.php';
+include_once 'NCBSCalendar.php';
 
 $calendar = new NCBSCalendar( './oauth-credentials.json' );
-header( 'Location: ' . $calendar->redirectURL, False );
+header( 'Location:' . $calendar->redirectURL, False );
 
 exit;
 

b46d67c6e92a46f25033596f3bdb06bc19df77c4
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-09 13:55:23 +0530

    Admin only approves or reject the request.

--- bookmyvenue_admin_request_review_submit.php
+++ bookmyvenue_admin_request_review_submit.php
@@ -1,20 +1,20 @@
 <?php
-include_once( "header.php" );
-include_once( "database.php" );
+
+include_once "header.php" ;
+include_once "database.php";
+include_once 'tohtml.php';
+
+
+$whatToDo = $_POST['response'];
+$isPublic = $_POST['isPublic'];
 
 if( ! array_key_exists( 'events', $_POST ) )
 {
     echo printWarning( "You did not select any request" );
-    echo goBackToPageLink( "bootstrap_admin.php", "Go back" );
+    echo goBackToPageLink( "bookmyvenue_admin.php", "Go back" );
     exit(0);
 }
-
 $events = $_POST['events'];
-$whatToDo = $_POST['response'];
-$isPublic = $_POST['isPublic'];
-
-// Ask user if they want to put this request on public calendar.
-
 foreach( $events as $event )
 {
     $event = explode( '.', $event );

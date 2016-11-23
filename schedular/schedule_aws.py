f67e4a601aafd3dbc9284190b3e3eec701fbaf69
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-23 14:33:40 +0530

    This does provide a good matching.

--- schedular/schedule_aws.py
+++ schedular/schedule_aws.py
@@ -17,6 +17,7 @@ __status__           = "Development"
 
 import sys
 import os
+import math
 import mysql.connector
 import mysql
 import ConfigParser
@@ -69,10 +70,17 @@ def construct_flow_graph( aws ):
     g_.add_node( 'sink', pos = (10, 10) )
 
     # Each speaker gets his node.
-    speakers = aws.keys( )
-    for i, speaker in enumerate( speakers ):
-        g_.add_node( speaker, last_date = aws[speaker][0]['date'], pos = (1,3*i) )
-        g_.add_edge( 'source', speaker, capacity = 1.0 )
+    speakers = []
+    for i, speaker in enumerate( aws.keys() ):
+        lastDate = aws[ speaker][0]['date']
+        if lastDate:
+            # assert lastDate, "No last date found for speaker %s" % speaker 
+            g_.add_node( speaker, last_date = aws[speaker][0]['date'], pos = (1,3*i) )
+            g_.add_edge( 'source', speaker, capacity = 1, weight = 0 )
+            speakers.append( speaker )
+        else:
+            print( 'Warning: Could not find last AWS date for %s' % speaker )
+            print( '\t I am ignoring him' )
 
     # Now add mondays for next 20 weeks.
     today = datetime.date.today()
@@ -85,32 +93,61 @@ def construct_flow_graph( aws ):
         for j in range( 3 ):
             dateSlot = '%s,%d' % (monday, j)
             g_.add_node( dateSlot, date = monday, pos = (5, 10*(3*i + j)) )
-            g_.add_edge( dateSlot, 'sink', capacity = 1.0 )
+            g_.add_edge( dateSlot, 'sink', capacity = 1, weight = 0 )
             slots.append( dateSlot )
     
     # Now for each student, add potential edges.
-    for n in speakers:
+    idealGap = 357
+    for speaker in speakers:
         prevAWSDate = g_.node[ speaker ][ 'last_date' ]
         for slot in slots:
             date = g_.node[ slot ][ 'date' ]
-            # print( prevAWSDate, date )
+            assert date
             nDays = ( date - prevAWSDate ).days
-            if nDays > 357:
-                print( nDays )
-                ratio = 357.0 / nDays 
-                if ratio < 0.4:
-                    print('Candidate %s has not given her aws in long time.' % n)
-                    ratio = 1.0
-                g_.add_edge( n, slot, capacity = ratio )
+            weight = abs( nDays - idealGap ) / idealGap
+            g_.add_edge( speaker, slot, capacity = 1, weight = weight ) #int(weight) )
+
+def test_graph( graph ):
+    """Test that this graph is valid """
+    # Each edge must have a capcity and weight 
+    for u, v in graph.edges():
+        if 'capacity' not in  graph[u][v]:
+            print( 'Error: %s -> %s no capacity assigned' % (u, v) )
+        if 'weight' not in  graph[u][v]:
+            print( 'Error: %s -> %s no weight assigned' % (u, v) )
+
+def getMatches( res ):
+    result = defaultdict( list )
+    for u in res:
+        if 'source' == u:
+            continue
+        for v in res[u]:
+            if 'sink' == v:
+                continue
+            f = res[u][v]
+            if f > 0:
+                date, slow = v.split(',')
+                result[date].append( u )
+    return result
 
 def schedule( ):
     global g_
-    res = nx.max_flow(g_, 'source', 'sink' )
-    print( res )
+    test_graph( g_ )
+    res = nx.max_flow_min_cost( g_, 'source', 'sink' )
+    schedule = getMatches( res )
+    for date in  sorted(schedule):
+        print( "On date %s = %s" % (date, schedule[date] ))
+        print( "\t\t Previous %s" % str( 
+            [ g_.node[x]['last_date'].strftime('%Y-%m-%d') for x in schedule[date] ] )
+            )
 
 def main( ):
     aws = getAllAWS( )
     construct_flow_graph( aws )
+    try:
+        nx.write_dot( g_, 'aws.dot' )
+    except Exception as e:
+        print( 'Failed to write dot %s' % e )
     schedule( )
     pos = nx.get_node_attributes( g_, 'pos' )
     # nx.draw( g_, pos )

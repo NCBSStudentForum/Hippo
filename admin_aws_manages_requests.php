53bf00e76c5e81c9a53d5a99f117f212f0792d65
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-24 15:25:59 +0530

    Added  a todo list.

--- admin_aws_manages_requests.php
+++ admin_aws_manages_requests.php
@@ -2,12 +2,14 @@
 
 include_once 'header.php';
 include_once 'database.php';
+include_once 'tohtml.php';
 
 echo "<h3>Manage pending requests</h3>";
 
 $pendingRequests = getPendingAWSRequests( );
+echo "TODO: Add accepted!";
 foreach( $pendingRequests as $req )
-    echo arrayToTableHTML( $req, 'aws' );
+    echo arrayToVerticalTableHTML( $req, 'aws' );
 
 echo goBackToPageLink( "admin_aws.php", "Go back" );
 

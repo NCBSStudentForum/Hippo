e9d18a7109ead3969f123aeaa798351a33cb8493
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-24 17:24:48 +0530

    Diff class and added code to accept/reject user requests.
    Former-commit-id: 0dccdd40154a4370724593f3358750f921252f95
    Former-commit-id: 06bc81dedfd9936f5a37b35a9a6981da3fbd843e

--- admin_aws_manages_requests.php
+++ admin_aws_manages_requests.php
@@ -3,14 +3,59 @@
 include_once 'header.php';
 include_once 'database.php';
 include_once 'tohtml.php';
+include_once 'methods.php';
+require_once 'class.Diff.php';
 
 echo "<h3>Manage pending requests</h3>";
 
 $pendingRequests = getPendingAWSRequests( );
-echo "TODO: Add accepted!";
 foreach( $pendingRequests as $req )
-    echo arrayToVerticalTableHTML( $req, 'aws' );
+{
+    $speaker = $req['speaker'];
+    $date = $req['date'];
+    $aws = getMyAwsOn( $speaker, $date );
+
+    $diff = array( );
+    foreach( $aws as $key => $val )
+        if( array_key_exists( $key, $req ) )
+            $diff[ $key ] = Diff::compare( $aws[$key], $req[$key] );
+
+    echo '<form method="post" action="admin_aws_manages_requests_submit.php">';
+    echo arrayToVerticalTableHTML( $req, 'aws', ''
+        , array( 'id', 'status', 'modified_on' ) 
+    );
+
+    echo '<table class="show_aws" border="0">
+        <tr style="background:white">
+            <td style="border:0px;min-width:50%;align:left;">
+                <button type="submit" name ="response" value="Reject">Reject</button>
+            </td>
+            <td style="border:0px;max-width:50%;">
+                <button type="submit" name ="response" value="Accept">Accept</button>
+            </td>
+        </tr>
+    </table>';
+    echo '<input type="hidden" name="request_id" value="' . $req['id'] . '" >';
+    echo '<input type="hidden" name="speaker" value="' . $speaker . '" >';
+    echo '<input type="hidden" name="date" value="' . $date . '" >';
+    echo '</form>';
+
+    // This button show changes made by user.
+    echo '
+        <button type="submit" onclick="show_changes()">Show chanages</button>
+        ';
+}
 
 echo goBackToPageLink( "admin_aws.php", "Go back" );
 
 ?>
+
+<script type="text/javascript" charset="utf-8">
+    function show_changes( )
+    {
+        var diff = <?php echo json_encode( $diff ) ?>;
+        alert( "TODO: Diff is" + diff );
+    }
+</script>
+
+

db8d4a22f2f75ca5c2b02b4fefc4ec82cc3b6764
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-25 23:08:15 +0530

    Added search function in AWS database.

--- user_aws_search.php
+++ user_aws_search.php
@@ -13,19 +13,22 @@ echo '
     </form>
     ';
 
-$query = $_GET['query' ];
-echo printInfo( "Searching for $query" );
-
-$awses = queryAWS( $query );
-echo printInfo( "Total matches " .  count( $awses ) );
-foreach( $awses as $aws )
+if( isset( $_GET[ 'query' ] ) )
 {
-    echo arrayToVerticalTableHTML( $aws, 'show_aws', ''
-    , array( 'id', 'time', 'supervisor_2'
-        , 'tcm_member_1', 'tcm_member_2', 'tcm_member_3', 'tcm_member_4'
+    $query = $_GET['query' ];
+    echo printInfo( "Searching for $query" );
+
+    $awses = queryAWS( $query );
+    echo printInfo( "Total matches " .  count( $awses ) );
+    foreach( $awses as $aws )
+    {
+        echo arrayToVerticalTableHTML( $aws, 'show_aws', ''
+            , array( 'id', 'time', 'supervisor_2'
+            , 'tcm_member_1', 'tcm_member_2', 'tcm_member_3', 'tcm_member_4'
         )
     );
-    echo '<br>';
+        echo '<br>';
+    }
 }
 
 echo goBackToPageLink( "user_aws.php", "Go back" );

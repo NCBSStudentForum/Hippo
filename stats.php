d79dfe0731c3ad0cc4ec15d676a6a87cae57d5c5
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-28 20:45:21 +0530

    Added highchart js.

--- stats.php
+++ stats.php
@@ -3,6 +3,14 @@
 include_once 'header.php';
 include_once 'database.php';
 
+?>
+
+<script type="text/javascript" charset="utf-8">
+    
+</script>
+
+
+<?php
 
 echo  goBackToPageLink( "index.php", "Go back" );
 

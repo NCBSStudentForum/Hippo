dcf59b7c7034e62eba3877b383cf6d398c6c480b
Dilawar Singh <dilawars@ncbs.res.in>
2016-11-29 17:02:59 +0530

    Remove duplicate entries now.
    Former-commit-id: e964c8eebaf2347642d3b796b8f1f2c4233f6303
    Former-commit-id: 974f37c5d71058597e1e45ae19a8b99775a33f8a

--- admin_aws_edit.php
+++ admin_aws_edit.php
@@ -10,7 +10,13 @@ mustHaveAllOfTheseRoles( array( 'AWS_ADMIN' ) );
 
 echo userHTML( );
 
+$aws = getMyAwsOn( $_POST[ 'speaker' ], $_POST[ 'date' ] );
+
+echo printWarning( "Currently this feature is not implemented. Only speaker of 
+    this AWS may be able to edit it. Please inform the speaker to edit details.
+    " );
 
 echo goBackToPageLink( $_SERVER['HTTP_REFERER'], 'Go back' );
+
 
 ?>

13deb321bd60773451426fab2027a6b48d4a6d6e
cal <cal@calendar.(none)>
2017-01-11 09:23:50 +0530

    Renamed to ncbs-hippo.
    Former-commit-id: 4365fa94123e1d0ef1d17a891cff5b717e2c6732
    Former-commit-id: fa9a86a2e38fc54b4955fffcde38563fb769a08a

--- header.php
+++ header.php
@@ -20,11 +20,11 @@ ini_set( "display_errors", 1 );
 </head>
 <div class="header">
 
-<title>NCBS Minion</title>
+<title>NCBS Hippo</title>
 
-<h1><a href="index.php">NCBS Minion</a></h1>
-<a href="https://github.com/dilawar/ncbs-minion" target='_blank'>Github</a>
-<a href='https://github.com/dilawar/ncbs-minion/issues' target="_blank">Report issue</a></small>
+<h1><a href="index.php">NCBS Hippo</a></h1>
+<a href="https://github.com/dilawar/ncbs-hippo" target='_blank'>Github</a>
+<a href='https://github.com/dilawar/ncbs-hippo/issues' target="_blank">Report issue</a></small>
 </div>
 <br />
 <br />

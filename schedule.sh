ce0868049f484117f5143fca82bac957ed10acfc
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-05 17:31:27 +0530

    A wrapper to to make sure that we use python2.7.

---
+++ schedule.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: schedule.sh
+#
+#         USAGE: ./schedule.sh
+#
+#   DESCRIPTION: 
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Dilawar Singh (), dilawars@ncbs.res.in
+#  ORGANIZATION: NCBS Bangalore
+#       CREATED: 02/05/2017 05:27:07 PM
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ -f /opt/rh/python27/enable ]; then
+    source /opt/rh/python27/enable 
+fi
+
+python2.7  $DIR/schedule_aws.py

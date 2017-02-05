9088749d6529669583be095eb42e0c37def5971b
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-05 15:50:36 +0530

    Using mutt to send emails.

--- mail.php
+++ mail.php
@@ -1,35 +1,25 @@
 <?php
+
 include('html2text.php');
 
-/**
- * Convert html to text and then send a plain text msg.
- */
-function sendEmail($msg, $ldap) 
+function sendEmailWithSub($msg, $sub, $to) 
 {
-	$msg = html2text($msg);
-	$to = $ldap."@iitb.ac.in";
-	$cmd="echo '$msg' | mutt -s 'Your details have been edited/updated successfully' -e 'set copy=no'";
-	$arg1=" -e \"set smtp_url=\"smtp://sandesh.ee.iitb.ac.in:25\"\"";
-	$arg2=" -e \"set from=\"noreply@ee.iitb.ac.in\"\"";
-	$arg3=" -e \"set realname=\"EE-Minion\"\"";
-	$arg4=" -e \"set content_type=text/html\"";
-	$arg5=" '$to' 2>&1";
-	exec($cmd.$arg1.$arg2.$arg3.$arg5, $out);
-	return $out;
+    $msg .= "<p>
+        This is a computer generated email, you need not reply. In case of 
+        any query, please write to acadoffice@ncbs.res.in.  </p>
+
+        <p>If you are not the intended recipeint of this email, please write 
+        to acadoffice@ncbs.res.in . </p>
+        ";
+
+    $msg = html2text($msg);
+    $muttfile = __DIR__ . "/muttrc";
+    $cmd="echo '$msg' | mutt -F $muttfile -s '$sub' $to";
+    echo( "Executing $cmd" );
+    exec( $cmd, $out);
+    return $out;
 }
 
-function sendEmailWithSub($msg, $sub, $ldap) 
-{
-	$msg = html2text($msg);
-	$to = $ldap."@iitb.ac.in";
-	$cmd="echo '$msg' | mutt -s '$sub' -e 'set copy=no'";
-	$arg1=" -e \"set smtp_url=\"smtp://sandesh.ee.iitb.ac.in:25\"\"";
-	$arg2=" -e \"set from=\"noreply@ee.iitb.ac.in\"\"";
-	$arg3=" -e \"set realname=\"EE-Minion\"\"";
-	$arg4=" -e \"set content_type=text/html\"";
-	$arg5=" '$to' 2>&1";
-	exec($cmd.$arg1.$arg2.$arg3.$arg5, $out);
-	return $out;
-}
+$res = sendEmailWithSub( "testing", "Your request has been created", "dilawars@ncbs.res.in" );
 
 ?>

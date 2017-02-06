e3dae6a764d2978e6713899f11d8853c74dab751
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-06 12:47:03 +0530

    Updated with new credentials.
    Former-commit-id: 564bd5c5d316880a235eb202105c6a078d46c961
    Former-commit-id: dbdad52e6719b2c0d29aee18a9559561ae14ee0b

--- index.php
+++ index.php
@@ -28,13 +28,16 @@ if(!$conf)
 $_SESSION['conf'] = $conf;
 
 $_SESSION['user'] = 'anonymous'; // This for testing purpose.
-$_SESSION[ 'oauth_credential' ] = __DIR__ . '/oauth-credentials.json';
-$_SESSION[ 'calendar_id'] = '6bvpnrto763c0d53shp4sr5rmk@group.calendar.google.com';
+$_SESSION[ 'oauth_credential' ] = __DIR__ . 
+    '/client_secret_636127149215-mn7vk37265hlq48d39qt45asnsvdbti0.apps.googleusercontent.com.json';
+$_SESSION[ 'calendar_id'] = 
+    'd2jud2r7bsj0i820k0f6j702qo@group.calendar.google.com'; 
+
 $_SESSION[ 'timezone' ] = 'Asia/Kolkata';
 
 ini_set( 'date.timezone', 'Asia/Kolkata' );
 ini_set( 'log_errors', 1 );
-ini_set( 'error_log', '/tmp/__minion__.log' );
+ini_set( 'error_log', '/tmp/__hippo.log' );
 
 /* counter */
 

0ba5b8f685d19949f9a80a098ee3d222509dac88
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-06 12:47:03 +0530

    Updated with new credentials.

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -14,7 +14,7 @@ include_once( 'tohtml.php' );
 function calendarURL( ) 
 {
     return '
-<iframe src="https://calendar.google.com/calendar/embed?src=6bvpnrto763c0d53shp4sr5rmk%40group.calendar.google.com&ctz=Asia/Calcutta" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+        <iframe src="https://calendar.google.com/calendar/embed?src=d2jud2r7bsj0i820k0f6j702qo%40group.calendar.google.com&ctz=Asia/Calcutta" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
     ';
 }
 

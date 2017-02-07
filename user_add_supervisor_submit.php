8119ca53a478f77f9ab0f2c9b76869362fcdd109
hippo <hippo@Hippo.(none)>
2017-02-07 18:13:49 +0530

    When a request is rejected, send email to user. Implemented.

--- user_add_supervisor_submit.php
+++ user_add_supervisor_submit.php
@@ -5,30 +5,40 @@ include_once( "methods.php" );
 include_once( 'tohtml.php' );
 include_once( "check_access_permissions.php" );
 
-try {
-    $res = insertIntoTable( 'supervisors'
-        , Array( "email", "first_name", "last_name", "affiliation", "url" )
-        , $_POST 
-    );
-} catch ( PDOException $e ) {
-
-    echo printWarning( "Could not add supervisor" );
-    echo printWarning( "\t Error was " . $e->getMessage( ) );
-    echo goBackToPageLink( "user_aws_edit_request.php", "Go back to AWS" );
-    exit( 0 );
-}
-
-
-if( $res )
+if( trim( $_POST[ 'email'] ) && trim( $_POST[ 'first_name' ] ) )
 {
-    echo printInfo( "Successfully added supervisor to list" );
-    goToPage( "user_add_supervisor.php", 1 );
-    exit;
+
+    try {
+        $res = insertIntoTable( 'supervisors'
+            , Array( "email", "first_name", "last_name", "affiliation", "url" )
+            , $_POST 
+        );
+    } catch ( PDOException $e ) {
+
+        echo printWarning( "Could not add supervisor" );
+        echo printWarning( "\t Error was " . $e->getMessage( ) );
+        echo goBackToPageLink( "user_aws_edit_request.php", "Go back to AWS" );
+        exit( 0 );
+    }
+
+    if( $res )
+    {
+        echo printInfo( "Successfully added supervisor to list" );
+        goToPage( "user_add_supervisor.php", 1 );
+        exit;
+    }
+    else
+    {
+        echo printWarning( "Could not add supervisor" );
+    }
 }
 else
 {
-    echo printWarning( "Could not add supervisor" );
+    echo printWarning( "Incomplete details. Try again .." );
+    goToPage( "user_add_supervisor.php", 1 );
+    exit;
 }
+
 
 echo goBackToPageLink( "user_aws_edit_request.php", "Go back to AWS" );
 

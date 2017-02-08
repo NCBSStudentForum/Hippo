9ab1bce8d876da473b099de6fd030e1eb325874c
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-08 11:41:39 +0530

    running on local server.
    Former-commit-id: 7bcec79bb186b4e4d798f76705b0bee98e19ae82
    Former-commit-id: f6cc9a62ba95ed7320778abcff445a9050ccdffa

--- admin_aws_add_aws_entry.php
+++ admin_aws_add_aws_entry.php
@@ -10,4 +10,14 @@ echo userHTML( );
 
 echo '<h3>Add new AWS entry</h3>';
 
+// We are using a generic function to create table. We need to add user name as  
+// well.
+
+echo '<form method="post" action=""> ';
+echo editableAWSTable( );
+echo '</form>';
+
+echo goBackToPageLink( 'aws_admin.php', 'Go back' );
+exit(0);
+
 ?>
--- user_aws_edit_request.php
+++ user_aws_edit_request.php
@@ -33,77 +33,7 @@ echo "<h3>Edit or add AWS entry</h3>";
 
 echo alertUser( "Supervisor 1 must be a local faculty. Others can be from outside." );
 
-// Now create an entry
-$supervisors = getSupervisors( );
-$supervisorIds = Array( );
-$supervisorText = Array( );
-foreach( $supervisors as $supervisor )
-{
-    array_push( $supervisorIds, $supervisor['email'] );
-    $supervisorText[ $supervisor['email'] ] = $supervisor['first_name']
-        .  ' ' . $supervisor[ 'last_name' ] ;
-}
-
-echo "<form method=\"post\" action=\"user_aws_edit_request_submit.php\">";
-echo "<table class=\"input\">";
-echo '
-    <tr>
-        <td>Title</td>
-        <td><input type="text" class="long" name="title" value="' 
-            . __get__( $default, 'title', '') . '" /></td>
-    </tr>
-    <tr>
-        <td>Abstract </td>
-        <td><textarea id="abstract" name="abstract" rows="10" cols="40">' 
-            . __get__( $default, 'abstract', '' ) . '</textarea>
-            <script> CKEDITOR.replace( "abstract" ); </script>
-        </td>
-    </tr>';
-
-$addMissingUrl = '<a target="_blank" href="user_add_supervisor.php">Add missing member</a>';
-for( $i = 1; $i <= 2; $i++ )
-{
-    $name = "supervisor_$i";
-    $selected = __get__( $default, $name, "" );
-    echo '
-    <tr>
-        <td>Supervisor ' . $i . '<br></td>
-        <td>' . arrayToSelectList( $name, $supervisorIds , $supervisorText, FALSE, $selected );
-    if( $i == 2 )
-        echo $addMissingUrl;
-    echo '</td> </tr>';
-}
-for( $i = 1; $i <= 4; $i++ )
-{
-    $name = "tcm_member_$i";
-    $selected = __get__( $default, $name, "" );
-    echo '
-    <tr>
-        <td>Thesis Committee Member ' . $i . '<br></td>
-        <td>' . arrayToSelectList( $name, $supervisorIds , $supervisorText, FALSE, $selected) 
-        . $addMissingUrl . '</td>';
-    echo '</tr>';
-
-}
-    echo '
-    <tr>
-        <td>Date</td>
-        <td><input class="datepicker" type="date" name="date" id="" value="' . 
-            __get__($default, 'date', '' ) . '" /></td>
-    </tr>
-    <tr>
-        <td>Time</td>
-        <td><input class="timepicker" name="time" id="" value="16:00" /></td>
-    </tr>
-    <tr>
-        <td></td>
-        <td>
-            <input  name="awsid" type="hidden" value="' . $awsId . '"  />
-            <button class="submit" name=\"response\" value="submit">Submit</button>
-        </td>
-    </tr>
-    ';
-echo "</table>";
+echo editableAWSTable( $awsId );
 echo "</form>";
 
 

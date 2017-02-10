1b1037d70a30cefe4cfa21e9d128b371dfcea88c
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-10 13:54:36 +0530

    Fixed conersion.
    Former-commit-id: 8f2ef46937dd9e30fe1b1ff4eb0214a13f1a6b7d
    Former-commit-id: 3d841fc135f9d5d4dfff05d2658ff25edee616f6

---
+++ html2markdown.py
@@ -0,0 +1,18 @@
+"""html2markdown.py: 
+
+Convert html to markdown.
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2016, Dilawar Singh"
+__credits__          = ["NCBS Bangalore"]
+__license__          = "GNU GPL"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+import sys
+import os
+import markdown2 

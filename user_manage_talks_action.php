13a136eb5976d6325ae83ff2b0593459d45dfa83
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-12 17:14:11 +0530

    Time to schedule a talk.
    Former-commit-id: 0adc5f8c085c7663455e631c88246fd5a6fe3bce
    Former-commit-id: c80c1acdb6af2c18b7bb8c202c7e6b3eb001821a

---
+++ user_manage_talks_action.php
@@ -0,0 +1,45 @@
+<?php
+
+include_once 'header.php';
+include_once 'database.php';
+include_once 'mail.php';
+include_once 'tohtml.php';
+include_once './check_access_permissions.php';
+mustHaveAnyOfTheseRoles( array( 'USER' ) );
+
+
+if( ! $_POST[ 'response' ] )
+{
+    // Go back to previous page.
+    goToPage( $_SERVER[ 'HTTP_REFERER' ], 0 );
+    exit;
+}
+else if( $_POST[ 'response' ] == 'delete' )
+{
+    $res = deleteFromTable( 'talks', 'id', $_POST );
+    if( $res )
+    {
+        echo printInfo( 'Successfully delete entry' );
+        echo goToPage( $_SERVER[ 'HTTP_REFERER' ], 0 );
+        exit;
+    }
+    else
+        echo printWarning( "Failed to delete the talk " );
+}
+else if( $_POST[ 'response' ] == 'edit' )
+{
+    echo printInfo( "Here you can only change the host, title and description
+        of the talk." );
+
+    $id = $_POST[ 'id' ];
+    $talk = getTableEntry( 'talks', 'id', $_POST );
+
+    echo '<form method="post" action="user_manage_talks_action_update.php">';
+    echo dbTableToHTMLTable('talks', $talk, 'host,title,description', 'update');
+    echo '</form>';
+}
+
+echo goBackToPageLink( "user_manage_talk.php", "Go back" );
+exit;
+
+?>
---
+++ user_register_talk_action.php
@@ -0,0 +1,45 @@
+<?php
+
+include_once 'header.php';
+include_once 'database.php';
+include_once 'mail.php';
+include_once 'tohtml.php';
+
+//var_dump( $_POST );
+
+// Here I get both speaker and talk details. I need a function which can either 
+// insert of update the visitor table. Other to create a entry in talks table.
+
+$res1 = insertOrUpdateTable( 'visitors'
+    , 'email,first_name,middle_name,last_name,department,institute,homepage'
+    , 'department,institute,homepage,email'
+    , $_POST 
+    );
+
+if( $res1 )
+{
+    // Assign speaker id from previous query.
+    $res1[ 'id' ] = $res1[ 'LAST_INSERT_ID()' ];
+    $speaker = getTableEntry( 'visitors', 'id', $res1 );
+    $speakerText = loginToText( $speaker );
+    $_POST[ 'speaker' ] = $speakerText;
+    $res2 = insertIntoTable( 'talks'
+        , 'host,title,speaker,description,created_by'
+        , $_POST ); 
+
+    if( $res2 )
+    {
+        echo printInfo( "Successfully registered your talk." );
+        goToPage( "user.php", 1 );
+        exit;
+    }
+    else
+        echo printWarning( "Oh Snap! Failed to add your talk to database." );
+}
+else
+    echo printWarning( "Oh Snap! Failed to add speaker to database" );
+
+echo goBackToPageLink( "user.php", "Go back" );
+exit;
+
+?>

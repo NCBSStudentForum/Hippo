224477b074af1a0890a040618834a3093847f258
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-14 10:23:37 +0530

    Updated some text.
    Former-commit-id: 8177e807356e28e0dbde60295c308998742e8bf7
    Former-commit-id: ec0b88c251579214edcadeb0e9997513d7350fa3

--- user_show_requests.php
+++ user_show_requests.php
@@ -14,28 +14,33 @@ echo "<br>";
 
 $requests = getRequestOfUser( $_SESSION['user'], $status = 'PENDING' );
 
-echo "<div stle>";
-foreach( $requests as $request )
+if( count( $requests ) < 1 )
+    echo printInfo( "No pending request found" );
+else
 {
-    $tobefiltered = Array( 
-        'rid', 'modified_by', 'timestamp', 'user', 'url' , 'status'
+    echo "<div stle>";
+    foreach( $requests as $request )
+    {
+        $tobefiltered = Array( 
+            'rid', 'modified_by', 'timestamp', 'user', 'url' , 'status'
         );
-    $gid = $request['gid'];
-    echo "<table class=\"info\" >";
-    echo "<tr>";
-    echo "<td>" . arrayToTableHTML( $request, "requests", NULL, $tobefiltered );
-    echo '<form method="post" action="user_show_requests_edit.php">';
-    echo "</td><td><button name=\"response\" title=\"Edit this request\"
-        value=\"edit\"> $symbEdit </button>";
-    echo "</td><td><button name=\"response\" title=\"Cancel this request\"
-        onclick=\"AreYouSure( this )\" > $symbCancel </button>";
-    echo "</td></tr>";
-    echo "</table>";
-    echo "<input type=\"hidden\" name=\"gid\" value=\"$gid\">";
-    echo '</form>';
+        $gid = $request['gid'];
+        echo "<table class=\"info\" >";
+        echo "<tr>";
+        echo "<td>" . arrayToTableHTML( $request, "requests", NULL, $tobefiltered );
+        echo '<form method="post" action="user_show_requests_edit.php">';
+        echo "</td><td><button name=\"response\" title=\"Edit this request\"
+            value=\"edit\"> $symbEdit </button>";
+        echo "</td><td><button name=\"response\" title=\"Cancel this request\"
+            onclick=\"AreYouSure( this )\" > $symbCancel </button>";
+        echo "</td></tr>";
+        echo "</table>";
+        echo "<input type=\"hidden\" name=\"gid\" value=\"$gid\">";
+        echo '</form>';
+    }
+    echo "<div>";
 }
 
-echo "<div>";
 echo goBackToPageLink( "user.php", "Go back" );
 
 ?>

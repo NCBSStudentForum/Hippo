c97f51787cebf1c2c887ab54b2c01e1ca2cb7e6e
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-14 19:04:27 +0530

    User can modify his/her AWS.

--- user_aws_update_upcoming_aws.php
+++ user_aws_update_upcoming_aws.php
@@ -6,6 +6,13 @@ mustHaveAllOfTheseRoles( array( 'USER' ) );
 include_once 'database.php';
 include_once 'tohtml.php';
 
+echo userHTML( );
+
+echo "<h3>You are updating your upcoming AWS </h3>";
+echo alertUser( "If you can't find supervisors/TCM members in drop down menu,
+    you'd have to go back and add them. The facility to do so is in your home 
+    page (<tt>My Home</tt> link in top-right corner)." );
+
 if( $_POST[ 'response' ] == 'update' )
 {
     $awsId = $_POST[ 'id' ];

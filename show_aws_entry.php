8fe6f147ec2d1ebd5564f18bcf3743b4895a9658
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-15 17:43:08 +0530

    Show both upcoming and recent AWS.
    Former-commit-id: a352d5478da378d67bd3d887764a0a95a52e726d
    Former-commit-id: 7bd276c8de80982d667faad9ca8a5fef15db9757

--- show_aws_entry.php
+++ show_aws_entry.php
@@ -34,6 +34,8 @@ echo '
 $whichDay = $default[ 'date' ];
 
 $awses = getTableEntries( 'annual_work_seminars', 'date' , "date='$whichDay'" );
+$upcoming = getTableEntries( 'upcoming_aws', 'date', "date='$whichDay'" );
+$awses = array_merge( $awses, $upcoming );
 
 if( count( $awses ) < 1 )
 {

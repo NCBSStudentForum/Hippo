6dee2ef71c4483f6a8daa490d8228425b2502849
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-16 12:15:18 +0530

    more cleanup.

--- bookmyvenue_admin_edit.php
+++ bookmyvenue_admin_edit.php
@@ -3,6 +3,10 @@
 include_once ("header.php" );
 include_once( "database.php" );
 include_once( "tohtml.php" );
+include_once 'check_access_permissions.php';
+
+mustHaveAllOfTheseRoles( array( 'BOOKMYVENUE_ADMIN' ) );
+echo userHTML( );
 
 $gid = $_POST['gid'];
 $eid = $_POST['eid'];
@@ -11,14 +15,13 @@ if( strcasecmp($_POST['response'], 'edit' ) == 0 )
 {
     // Get a representative event of this group.
     $event = getEventsById( $gid, $eid );
-    echo printInfo( "Chaging event $gid . $eid " );
+    echo printInfo( "Chaging following event $gid . $eid " );
     echo '<form method="post" action="bookmyvenue_admin_edit_submit.php">';
     echo dbTableToHTMLTable( 'events'
         , $defaults = $event
         , $editables = Array( 
             'status', 'class', 'is_public_event'
             , 'title', 'description'
-            //, 'date', 'start_time', 'end_time' 
         ));
     echo "</form>";
 }

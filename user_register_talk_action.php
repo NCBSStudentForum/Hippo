98f49951fd1d7280a51449b411eb440ed9778d5a
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-17 12:10:59 +0530

    Stop user from creating invalid entry.

--- user_register_talk_action.php
+++ user_register_talk_action.php
@@ -8,77 +8,89 @@ include_once 'tohtml.php';
 // Here I get both speaker and talk details. I need a function which can either 
 // insert of update the speaker table. Other to create a entry in talks table.
 
-$res1 = insertOrUpdateTable( 'speakers'
-    , 'email,first_name,middle_name,last_name,department,institute,homepage'
-    , 'department,institute,homepage,email'
-    , $_POST 
+// Sanity check 
+if( ! ( $_POST['first_name']  && _POST[ 'institute' ] && _POST[ 'title' ] 
+    && $_POST[ 'description' ] ) )
+{
+    echo printInfo( 'Incomplete entry. Required fields: First name, last name, 
+        institute, title and description of talk. ' );
+    echo arrayToVerticalTableHTML( $_POST, 'info' );
+}
+else                // Everything is fine.
+{
+
+    $res1 = insertOrUpdateTable( 'speakers'
+        , 'email,first_name,middle_name,last_name,department,institute,homepage'
+        , 'department,institute,homepage,email'
+        , $_POST 
     );
 
-if( $res1 )
-{
-    // Assign speaker id from previous query.
-    $res1[ 'id' ] = $res1[ 'LAST_INSERT_ID()' ];
-    $speaker = getTableEntry( 'speakers', 'id', $res1 );
-
-    // This entry may be used on public calendar. Putting email anywhere on 
-    // public domain is allowed.
-    $speakerText = loginToText( $speaker, $withEmail = False  );
-    $_POST[ 'speaker' ] = $speakerText;
-    $res2 = insertIntoTable( 'talks'
-        , 'host,title,speaker,description,created_by'
-        , $_POST ); 
-
-    if( $res2 )
+    if( $res1 )
     {
-        echo printInfo( "Successfully registered your talk." );
-        $startTime = $_POST[ 'start_time' ];
-        $endTime = $_POST[ 'end_time' ];
-        $date = $_POST[ 'end_time' ];
-        $venue = $_POST[ 'venue' ];
+        // Assign speaker id from previous query.
+        $res1[ 'id' ] = $res1[ 'LAST_INSERT_ID()' ];
+        $speaker = getTableEntry( 'speakers', 'id', $res1 );
 
-        $reqs = getRequestsOnThisVenueBetweenTime( $venue, $date
-            , $startTime, $endTime );
-        $events = getEventsOnThisVenueBetweenTime( $venue, $date
-            , $startTime, $endTime );
-        if( $reqs || $events )
+        // This entry may be used on public calendar. Putting email anywhere on 
+        // public domain is allowed.
+        $speakerText = loginToText( $speaker, $withEmail = False  );
+        $_POST[ 'speaker' ] = $speakerText;
+        $res2 = insertIntoTable( 'talks'
+            , 'host,title,speaker,description,created_by'
+            , $_POST ); 
+
+        if( $res2 )
         {
-            echo printInfo( "There is already an events on $venue on $date
-                between $startTime and $endTime. 
-                <br />
-                I am redirecting you to page where you can create booking reqest
-                after exploring possible options.  "
-            );
-            goToPage( 'user_manage_talk.php', 3 );
-            exit;
-        }
+            echo printInfo( "Successfully registered your talk." );
+            $startTime = $_POST[ 'start_time' ];
+            $endTime = $_POST[ 'end_time' ];
+            $date = $_POST[ 'end_time' ];
+            $venue = $_POST[ 'venue' ];
 
-        if( $venue && $startTime && $endTime && $date )
-        {
-            // Else create a request.
-            $external_id = "taks." . $res1[ 'id' ];
-            $_POST[ 'external_id' ] = $external_id;
-            $_POST[ 'is_public_event' ] = 'YES';
-
-            // Modify talk title for calendar.
-            $_POST[ 'title' ] = "Talk by " . $_POST[ 'speaker' ] . ' on \'' . 
-                trim( $_POST[ 'title' ] ) . "'";
-
-            $res = submitRequest( $_POST );
-            if( $res )
+            $reqs = getRequestsOnThisVenueBetweenTime( $venue, $date
+                , $startTime, $endTime );
+            $events = getEventsOnThisVenueBetweenTime( $venue, $date
+                , $startTime, $endTime );
+            if( $reqs || $events )
             {
-                echo printInfo( "Successfully created booking request" );
-                goToPage( "user.php", 2 );
+                echo printInfo( "There is already an events on $venue on $date
+                    between $startTime and $endTime. 
+                    <br />
+                    I am redirecting you to page where you can create booking reqest
+                    after exploring possible options.  "
+                );
+                goToPage( 'user_manage_talk.php', 3 );
                 exit;
             }
-            else
-                echo printWarning( "Oh Snap! Failed to create booking request" );
+
+            if( $venue && $startTime && $endTime && $date )
+            {
+                // Else create a request.
+                $external_id = "taks." . $res1[ 'id' ];
+                $_POST[ 'external_id' ] = $external_id;
+                $_POST[ 'is_public_event' ] = 'YES';
+
+                // Modify talk title for calendar.
+                $_POST[ 'title' ] = "Talk by " . $_POST[ 'speaker' ] . ' on \'' . 
+                    trim( $_POST[ 'title' ] ) . "'";
+
+                $res = submitRequest( $_POST );
+                if( $res )
+                {
+                    echo printInfo( "Successfully created booking request" );
+                    goToPage( "user.php", 2 );
+                    exit;
+                }
+                else
+                    echo printWarning( "Oh Snap! Failed to create booking request" );
+            }
         }
+        else
+            echo printWarning( "Oh Snap! Failed to add your talk to database." );
     }
     else
-        echo printWarning( "Oh Snap! Failed to add your talk to database." );
+        echo printWarning( "Oh Snap! Failed to add speaker to database" );
 }
-else
-    echo printWarning( "Oh Snap! Failed to add speaker to database" );
 
 echo goBackToPageLink( "user.php", "Go back" );
 

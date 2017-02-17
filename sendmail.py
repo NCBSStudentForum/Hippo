679d2fe51018d0e45372da0fe8e7effe7cde1977
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-17 11:55:19 +0530

    I can send emails now.

--- sendmail.py
+++ sendmail.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import smtplib 
 import html2text
 from email.mime.text import MIMEText
+import logging
+
+logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        datefmt='%m-%d %H:%M',
+        filename='/var/log/hippo.log',
+        filemode='a'
+        )
+
+_logger = logging.getLogger('hippo.sendmail')
 
 if len( sys.argv ) < 4:
-    print( "Usage: %s TO SUBJECT MSG_FILE" % sys.argv[0] )
+    _logger.warn( "Usage: %s TO SUBJECT MSG_FILE" % sys.argv[0] )
     quit( )
 
 fromAddress = 'NCBS Hippo <noreply@ncbs.res.in>'
 toAddr = sys.argv[1]
 subject = sys.argv[2]
+
+_logger.debug( "Got command line params %s" % sys.argv )
 
 msg = '''Error: no text found'''
 with open( sys.argv[3], 'r' )  as f:
@@ -24,4 +38,14 @@ msg[ 'From' ] = 'NCBS Hippo <noreply@ncbs.res.in>'
 s = smtplib.SMTP( 'mail.ncbs.res.in', 587 )
 s.set_debuglevel( 2 )
 
-s.sendmail( fromAddress, toAddr.split( ',' ), msg.as_string( ) )
+try:
+    _logger.info( 'Sending email to %s' % toAddr )
+    s.sendmail( fromAddress, toAddr.split( ',' ), msg.as_string( ) )
+    # remove the mail
+    os.remove( sys.argv[3] )
+except Exception as e:
+    with open( '/var/log/hippo.log', 'a' ):
+        _logger.error( 'Failed to send email. Error was %s' % e )
+
+_logger.info( "Everything went OK. Mail sent sucessfully" )
+s.quit( )

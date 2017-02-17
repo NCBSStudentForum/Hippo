1b286bd86d91a1a29362fde5cdc7698a2ca409a5
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-17 11:55:19 +0530

    I can send emails now.
    Former-commit-id: 679d2fe51018d0e45372da0fe8e7effe7cde1977
    Former-commit-id: b252a1a2934425c181aba5425363cb6dc2570c0e

--- mail.php
+++ mail.php
@@ -1,27 +1,25 @@
 <?php
 
-include('html2text.php');
-
 function sendEmail($msg, $sub, $to) 
 {
     $timestamp = date( 'r', strtotime( 'now' ) );
 
-    $msg .= "<p>
+    $msg .= "
+        <p>==========================================================</p>
+        <p>
         This is a computer generated email, you need not reply. In case of 
         any query, please write to acadoffice@ncbs.res.in.  </p>
 
         <p>If you are not the intended recipeint of this email, please write 
         to acadoffice@ncbs.res.in . </p>
-        <br />
         ";
 
-    $msg .= "Created on : $timestamp";
+    $msg .= "<p>Created on : $timestamp </p>";
     $msgfile = "/tmp/__msg__.html";
 
     file_put_contents( $msgfile, $msg );
     $to = trim( $to );
-    $cmd= __DIR__ . "/sendmail.py $to $sub $msgfile 2>&1 | tee /tmp/_sendmail.log_";
-    echo( "Executing $cmd" );
+    $cmd= __DIR__ . "/sendmail.py '$to' '$sub' '$msgfile' 2>&1 | tee /tmp/_sendmail.log_";
     $out = shell_exec( $cmd );
     return $out;
 }

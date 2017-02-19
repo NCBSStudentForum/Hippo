ab176c940cc6ee17c440bd0fef28e3e7f5650ec7
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-19 12:25:14 +0530

    Some cleanup in user page.

--- user_info.php
+++ user_info.php
@@ -9,21 +9,13 @@ $info = getUserInfo( $_SESSION['user'] );
 $picPath = $_SESSION[ 'conf' ]['data']['user_imagedir'] . 
     '/' . $_SESSION['user'] . '.png';
 
-echo "<h3>Your details from LDAP</h3>";
 
-echo arrayToVerticalTableHTML( $info, "user", ''
-   , Array( 'roles', 'status', 'institute', 'created_on', 'last_login',
-   'valid_until', 'alternative_email' )
-);
+echo alertUser( 
+    "<p>Make sure your <tt>TITLE</tt>, <tt>JOINED ON</tt> and picture are 
+    correct. </p>"
+    );
 
-echo "<h3>Edit details </h3>";
-
-echo printInfo( 
-    "<p>&#x26a0 Atleast, select your TITLE and JOINED ON date and 
-    update your picture. </p>" 
-);
-
-echo '<table class="editable_user_picture">';
+echo '<table class="">';
 echo '<tr><td>';
 
 if( file_exists( $picPath ) )
@@ -37,19 +29,18 @@ else
         Please upload one."
     );
 }
-echo '</td><td>';
+echo '</td></tr><tr><td>';
 
 // Form to upload a picture
 echo '<form action="user_upload_picture.php" 
     method="post" 
     enctype="multipart/form-data">';
 
-echo '<p>
+echo '<p><small>
     This picture will be used in AWS notifications. It will be 
     rescaled to fit 5 cm x 5 cm space. I will not accept any picture bigger 
-    than 1MB in size. Allowed formats (PNG/JPG/GIF/BMP).
-    </p>
-    <br />
+    than 1MB in size. Allowed formats (PNG/JPG/GIF/BMP). 
+    </small></p>
     ';
 echo '<input type="file" name="picture" id="picture" value="" />';
 echo '<button name="Response" title="Upload your picture" value="upload">' 
@@ -57,7 +48,9 @@ echo '<button name="Response" title="Upload your picture" value="upload">'
 echo '</form>';
 echo '</td></tr>';
 echo '</table>';
+echo '<br>';
 
+// This is second table.
 echo "<form method=\"post\" action=\"user_info_action.php\">";
 echo dbTableToHTMLTable( 'logins', $info
     , $editables = Array( 'title', 'first_name', 'last_name'

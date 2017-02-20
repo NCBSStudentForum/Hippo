7a236c5f041b95d8612901bc3abd65e235ac5991
Dilawar Singh <dilawars@ncbs.res.in>
2017-02-20 19:22:54 +0530

    In case of at least one failure, it does not redirect user.
    Former-commit-id: a11fea3873b89ec71bc69e43e9e2bed7ffc98e33
    Former-commit-id: 72c594c2974f65e873b44f72308b63b505b7bb0a

--- bookmyvenue_admin_synchronize_events_with_google_calendar.php
+++ bookmyvenue_admin_synchronize_events_with_google_calendar.php
@@ -3,7 +3,6 @@
 include_once 'header.php';
 include_once 'database.php';
 
-
 // Before calling this script, the user must set 
 // $_SESSION['google_calendar_command'] variable. Accordingly 
 // ./oauthcallback.php file will handle the request. Most of the commands 
--- calendar/authenticate_gcalendar.php
+++ calendar/authenticate_gcalendar.php
@@ -1,17 +1,28 @@
 <?php 
 
 include_once __DIR__ . '/../check_access_permissions.php';
-mustHaveAllOfTheseRoles( array( 'BOOKMYVENUE_ADMIN' ) );
+mustHaveAllOfTheseRoles( array( 'BOOKMYVENUE_ADMIN', 'ADMIN' ) );
 
 include_once 'NCBSCalendar.php';
 
 $calendar = new NCBSCalendar( $_SESSION[ 'oauth_credential' ]
     , $_SESSION[ 'calendar_id' ] );
 
-// Check here if a user is logged in, if not she needs to be redirected back to 
-// admin with proper message.
-// TODO: Check if user has the proper authentication,
-header( 'Location:' . $calendar->redirectURL );
-exit;
+if( $calendar )
+{
+    // Check here if a user is logged in, if not she needs to be redirected back to 
+    // admin with proper message.
+    // TODO: Check if user has the proper authentication,
+    header( 'Location:' . $calendar->redirectURL );
+    exit;
+}
+else
+{
+    echo minionEmbarrassed( 
+        "Failed to created calendar instance. This is an error" 
+    );
+    echo goBackToPageLink( "bookmyvenue_admin.php", "Go back" );
+    exit;
+}
 
 ?>

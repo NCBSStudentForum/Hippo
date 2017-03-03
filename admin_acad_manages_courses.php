a14ec14816f8c0fa1244f8e6e1e79f1e17364b01
Dilawar Singh <dilawars@ncbs.res.in>
2017-03-03 14:50:20 +0530

    Admin and add courses.
    Former-commit-id: d587aeee49c872ef4a696390535e1fbe675d8480
    Former-commit-id: 08776837dfc2df72a35bc68c270aafbac219c925

---
+++ admin_acad_manages_courses.php
@@ -0,0 +1,85 @@
+<?php
+
+include_once 'check_access_permissions.php';
+mustHaveAnyOfTheseRoles( array( 'AWS_ADMIN' ) );
+
+include_once 'database.php';
+include_once 'tohtml.php';
+include_once 'methods.php';
+
+echo userHTML( );
+
+// Javascript.
+$courses = getTableEntries( 'courses' );
+
+$instructors = array();
+foreach( getFaculty( ) as $fac )
+    $instructors[ ] = $fac[ 'email' ];
+
+$coursesMap = array( );
+$coursesId = array_map( function( $x ) { return $x['id']; }, $courses );
+
+foreach( $courses as $course )
+    $coursesMap[ $course[ 'id' ] ] = $course;
+
+?>
+
+<script type="text/javascript" charset="utf-8">
+// Autocomplete speaker.
+$( function() {
+    var coursesDict = <?php echo json_encode( $coursesMap ) ?>;
+    var courses = <?php echo json_encode( $coursesId ); ?>;
+    var instructors = <?php echo json_encode( $instructors ); ?>;
+    $( "#course" ).autocomplete( { source : courses }); 
+    $( "#course" ).attr( "placeholder", "autocomplete" );
+    $( "input[id^=courses_instructor]" ).autocomplete( { source : instructors }); 
+    $( "input[id^=courses_instructor]" ).attr( "placeholder", "autocomplete" );
+});
+</script>
+
+<?php
+
+// Logic for POST requests.
+$course = array( 'id' => '', 'day' => '', 'start_time' => '', 'end_time' => '' );
+
+echo "<h2>Courses</h2>";
+echo coursesTable( );
+
+echo '<form method="post" action="">';
+echo '<input id="course" name="id" type="text" value="" >';
+echo '<button type="submit" name="response" value="show">Show details</button>';
+echo '</form>';
+
+
+// Show speaker image here.
+if( array_key_exists( 'id', $_POST ) )
+{
+    // Show emage.
+    $course = __get__( $coursesMap, $_POST['id'], null );
+    if( $course )
+        echo arrayToVerticalTableHTML( $course, 'course' );
+}
+
+echo '<h3>Add/Edit course details</h3>';
+
+echo '<form method="post" action="admin_acad_manages_courses_action.php">';
+
+
+echo dbTableToHTMLTable( 'courses', $course 
+    , 'id,credits,name,description,instructor_1,instructor_2,instructor_3' 
+        . ',instructor_4,instructor_5,instructor_6' 
+        . ',comment'
+    , 'submit' 
+    );
+
+echo '<button title="Delete this entry" type="submit" onclick="AreYouSure(this)"
+    name="response" value="Delete">' . $symbDelete .
+    '</button>';
+
+echo '</form>';
+
+
+echo "<br/><br/>";
+echo goBackToPageLink( 'admin_acad.php', 'Go back' );
+
+?>

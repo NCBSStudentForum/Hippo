1bdff67ab696f30f0e3e7bcc6c5bb631c5f08539
Dilawar Singh <dilawars@ncbs.res.in>
2017-03-07 17:40:05 +0530

    Ready for testing.
    Former-commit-id: 127aeee9f0bb9cb62aa7b3ea004e3c21244e7781
    Former-commit-id: a4d174c88b6e98f3376dd00a8d5738472680ba97

--- admin_acad_send_email.php
+++ admin_acad_send_email.php
@@ -8,10 +8,46 @@ include_once 'database.php';
 include_once 'tohtml.php';
 
 // We are here to send an email. 
+$email = array( );
+if( $_POST )
+{
+    $templ = json_decode( $_POST[ 'template' ], $assoc = true );
 
-$templ = json_decode( $_POST[ 'templalte' ] );
-$_POST[ 'msg' ] = $templ[ 'email_body'] ;
-echo dbTableToHTMLTable( 'emails', $_POST );
+    $email[ 'email_body' ] = html2Markdown( $templ[ 'email_body'], true );
+    $email[ 'recipients' ] = $templ[ 'recipients'] ;
+    $email[ 'cc' ] = $templ[ 'cc'] ;
+    $email[ 'subject'] = $_POST[ 'subject' ];
+
+    echo '
+        <form method="post" action="admin_acad_send_email_action.php">
+        <table class="tasks">
+        <tr>
+            <td>Recipients</td>
+            <td><input class="long" name="recipients" type="text" value="' . $email[ 'recipients' ] . '"></td>
+        </tr>
+        <tr>
+            <td>CC</td>
+            <td><input class="long" name="cc" type="text" value="' . $email[ 'cc' ] . '"></td>
+        </tr>
+        <tr>
+            <td>Subject</td>
+            <td><input class="long" name="subject" type="text" value="' . $email[ 'subject' ] . '"></td>
+        </tr>
+        <tr>
+            <td>Email body</td>
+            <td><textarea name="email_body" rows="20" cols="80">'
+                . $email[ 'email_body' ] . '</textarea></td>
+        </tr>
+        <tr><td></td>
+            <td> 
+                <button style="float:right" name="response" value="send">Send</button> 
+            </td>
+        </tr>
+        </table>
+        </form>'
+        ;
+}
+
 
 echo goBackToPageLink( 'admin_acad_email_and_docs.php', 'Go back' );
 

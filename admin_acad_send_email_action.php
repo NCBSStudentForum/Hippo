b262aa40733ec06be43dd48f248960f46ce0fca7
Dilawar Singh <dilawars@ncbs.res.in>
2017-03-07 17:40:18 +0530

    Ready for testing.
    Former-commit-id: 0efd24403654dc122eeda999217a76155269780d
    Former-commit-id: 9d885fd7e3926394396a8c105009210609752eda

---
+++ admin_acad_send_email_action.php
@@ -0,0 +1,35 @@
+<?php
+
+include_once 'check_access_permissions.php';
+mustHaveAnyOfTheseRoles( array( 'AWS_ADMIN', 'ADMIN' ) );
+
+include_once 'methods.php';
+include_once 'database.php';
+include_once 'tohtml.php';
+include_once 'mail.php';
+
+if( $_POST['response'] == 'DO_NOTHING' )
+{
+    echo printInfo( "User said do nothing.");
+    goBack( "admin_acad_email_and_docs.php", 0 );
+    exit;
+}
+else if( $_POST['response'] == 'send' )
+{
+    $to = $_POST[ 'recipients' ];
+    $msg = $_POST[ 'email_body' ];
+    $cclist = $_POST[ 'cc' ];
+    $subject = $_POST[ 'subject' ];
+    echo printInfo( "Sending email to $to ($cclist ) with subject $subject" );
+    $res = sendPlainTextEmail( $msg, $subject, $to, $cclist );
+    if( $res )
+        echo "Email sent successfully.";
+    else
+        echo minionEmbarrassed( "Failed to send email" );
+
+}
+
+echo goBackToPageLink( 'admin_acad_email_and_docs.php', 'Go back' );
+
+
+?>

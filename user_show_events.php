233cd2871829c44ca3a6e50af4db3a394b174854
Dilawar Singh <dilawars@ncbs.res.in>
2017-03-10 12:10:01 +0530

    Added description on user edit event page.

--- user_show_events.php
+++ user_show_events.php
@@ -12,24 +12,32 @@ echo userHTML( );
 <?php
 
 $events = getEventsOfUser( $_SESSION['user'] );
-
-foreach( $events as $event )
+if( count( $events ) < 1 )
+    echo alertUser( "No upcoming events." );
+else 
 {
-    $gid = $event['gid'];
-    echo '<form method="post" action="user_show_events_edit.php">';
-    echo arrayToVerticalTableHTML( $event, "info", 'is_public_event,eid' );
-    echo "<table class=\"info\">";
-    echo "<tr>";
-    echo "<td><button name=\"response\" title=\"Cancel this event\" 
-            onclick=\"AreYouSure(this)\" >" . $symbCancel . "</button>
-        </td>";
-    echo "<td style=\"float:right\">
-            <button title=\"Edit this event\" name=\"response\" 
-            value=\"edit\">" . $symbEdit . "</button> </td>";
-    echo "</tr>";
-    echo "</table>";
-    echo "<input type=\"hidden\" name=\"gid\" value=\"$gid\">";
-    echo '</form>';
+    echo "<h2>You have following upcoming events </h2>";
+    foreach( $events as $event )
+    {
+        $gid = $event['gid'];
+        echo '<form method="post" action="user_show_events_edit.php">';
+        echo arrayToVerticalTableHTML( $event, "info", ''
+            , 'last_modified_on,created_by,external_id,is_public_event,eid'
+             );
+        echo "<table class=\"show_info\">";
+        echo "<tr>";
+        echo "<td><button name=\"response\" title=\"Cancel this event\" 
+                onclick=\"AreYouSure(this)\" >" . $symbCancel . "</button>
+            </td>";
+        echo "<td style=\"float:right\">
+                <button title=\"Edit this event\" name=\"response\" 
+                value=\"edit\">" . $symbEdit . "</button> </td>";
+        echo "</tr>";
+        echo "</table>";
+        echo "<br>";
+        echo "<input type=\"hidden\" name=\"gid\" value=\"$gid\">";
+        echo '</form>';
+    }
 }
 
 echo goBackToPageLink( "user.php", "Go back" );

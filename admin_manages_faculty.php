65e7f0bcee3d621ea06e7495aae2124a59bb68fb
Dilawar Singh <dilawars@ncbs.res.in>
2017-03-12 16:43:33 +0530

    We can now update faculty like a sane person.
    Former-commit-id: a5c62c1a9504a785cc8f02868b49249eb0973723
    Former-commit-id: a6d6a30febcce57141c924d84a13dadef0055ad7

--- admin_manages_faculty.php
+++ admin_manages_faculty.php
@@ -6,16 +6,49 @@ include_once 'check_access_permissions.php';
 
 mustHaveAllOfTheseRoles( Array( 'ADMIN' ) );
 
-
 echo userHTML( );
 
-$faculty = getFaculty( );
+$default = array( );
+$action = 'Add';
 
-echo "<h3>Add a new faculty</h3>";
+$faculty = getFaculty( );
+$facultyMap = array( );
+foreach( $faculty as $fac )
+    $facultyMap[ $fac[ 'email' ] ] = $fac;
+
+$facultyEmails = array_keys( $facultyMap );
+
+?>
+
+<script type="text/javascript" charset="utf-8">
+$( function() {
+    var emails = <?php echo json_encode( $facultyEmails ) ?>;
+    $( "#faculty" ).autocomplete( { source : emails }); 
+    $( "#faculty" ).attr( "placeholder", "autocomplete" );
+});
+</script>
+
+<?php
+
+echo '<form method="post" action="">
+    Email of facutly <input id="faculty" name="faculty_email">
+    <button type="submit" name="response" value="search">Search</button>
+    </form>';
+
+if( array_key_exists( 'response', $_POST ) )
+{
+    $faculty = getTableEntry( 'faculty', 'email', array( 'email' => $_POST['faculty_email'] ) );
+    $default = array_merge( $default, $faculty );
+    $action = 'Update';
+}
+
+echo '<br/><br/>';
+
 echo '<form method="post" action="admin_manages_faculty_submit.php">';
-echo dbTableToHTMLTable( 'faculty', array()
+echo dbTableToHTMLTable( 'faculty'
+    , $default
     , array( 'email', 'first_name', 'middle_name', 'last_name'
-    , 'status', 'affiliation', 'url' ), 'add'
+    , 'status', 'affiliation', 'url' ), $action
 );
 echo "</form>";
 
@@ -23,14 +56,14 @@ echo "</form>";
 
 echo "<h3>Update existing faculty</h3>";
 
-foreach( $faculty as $fac )
-{
-    echo '<form method="post" action="admin_manages_faculty_submit.php">';
-    echo dbTableToHTMLTable( 'faculty', $fac
-        ,  array( 'first_name', 'middle_name', 'last_name'
-        , 'status', 'url', 'affiliation' ), 'edit' );
-    echo '</form>';
-}
+//foreach( $faculty as $fac )
+//{
+//    echo '<form method="post" action="admin_manages_faculty_submit.php">';
+//    echo dbTableToHTMLTable( 'faculty', $fac
+//        ,  array( 'first_name', 'middle_name', 'last_name'
+//        , 'status', 'url', 'affiliation' ), 'edit' );
+//    echo '</form>';
+//}
 
 echo goBackToPageLink( "admin.php", "Go back" );
 

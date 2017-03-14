2f9db46709f4d943d97f07ff8dae9d26b128f62e
Dilawar Singh <dilawars@ncbs.res.in>
2017-03-14 11:26:18 +0530

    User can register for courses.
    Former-commit-id: d76dfb8dc2985260b71fe36489219b74c0faa692
    Former-commit-id: a8117d33ba9411303015f29b9f18135ee393c1a7

--- user_manages_courses.php
+++ user_manages_courses.php
@@ -15,6 +15,70 @@ $year = getCurrentYear( );
 // User courses.
 $myCourses = getMyCourses( $sem, $year, $user = $_SESSION[ 'user' ] );
 
+$runningCourses = array( );
+foreach( getSemesterCourses( $year, $sem ) as $rc )
+    $runningCourses[ $rc[ 'course_id' ] ] = $rc;
+
+
+echo '<h3>Registered courses </h3>';
+
+
+echo alertUser( "
+    <strong>Policy for dropping a course </strong> 
+    <br>  Upto 30 days from starting of course, you are free to drop a course. 
+    After that, you need to write to your course instructor and academic office.
+    " );
+
+$tofilter = 'student_id';
+
+echo '<div style="font-size:small">';
+
+echo '<table border="1">';
+echo '<tr>';
+
+$action = 'drop';
+$count = 0;
+
+echo printInfo( "You are registered for following courses." );
+foreach( $myCourses as $c )
+{
+    $count += 1;
+    if( $count % 3 == 0 )
+        echo '</tr><tr>';
+
+    echo '<td>';
+    echo '<form method="post" action="user_manages_courses_action.php">';
+
+
+    $cid = $c[ 'course_id' ];
+    $course = getTableEntry( 'courses_metadata', 'id', array( 'id' => $cid ) );
+
+    // If more than 30 days have passed, do not allow registering for courses.
+    if( strtotime( 'now' ) - strtotime( $runningCourses[ $cid][ 'start_date' ] ) 
+            > 30 * 24 * 3600 )
+    {
+        echo $course['name'] . '<br>' . '<tt>' . 
+            humanReadableDate( $runningCourses[ $cid ][ 'start_date' ] ) 
+            . ' to ' . humanReadableDate( $runningCourses[$cid]['end_date'] )
+            . '</tt>'
+            ;
+
+        $action = ''; 
+    }
+
+    // TODO: Don't show grades unless student has given feedback.
+    if( strlen( $c[ 'grade' ] == 0 ) )
+        $tofilter .= ',grade,grade_is_given_on';
+
+    echo dbTableToHTMLTable( 'course_registration', $c, '', $action, $tofilter );
+    echo '</form>';
+    echo '</td>';
+}
+    
+echo '</tr></table>';
+echo '</div>';
+
+
 // Running course this semester.
 $runningCourses = getSemesterCourses( $year, $sem );
 
@@ -27,9 +91,9 @@ foreach( $runningCourses as $c )
     $courseMap[ $cid ] = $cid . ' - ' . getCourseName( $cid );
 }
 
-$courseSelect = arrayToSelectList( 'courses', $options, $courseMap );
+$courseSelect = arrayToSelectList( 'course_id', $options, $courseMap );
 
-echo "<h3>Register for courses </h3>";
+echo "<h3>Register for selected course </h3>";
 $default = array( 'student_id' => $_SESSION[ 'user' ] 
                 , 'semester' => $sem
                 , 'year' => $year
@@ -37,7 +101,8 @@ $default = array( 'student_id' => $_SESSION[ 'user' ]
                 , 'course_id' => $courseSelect
                 );
 
-echo '<form method="post" action="admin_acad_manages_action.php">';
+
+echo '<form method="post" action="user_manages_courses_action.php">';
 echo dbTableToHTMLTable( 'course_registration'
                         , $default
                         , 'course_id,type' 

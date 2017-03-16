cc33d1903aed7fe91bd13dd2ba8b53fdb379b8ac
Dilawar Singh <dilawars@ncbs.res.in>
2017-03-16 14:58:46 +0530

    Fixed the repeat pattern.
    Former-commit-id: cc9bf5c505c650b8603e6694a9e9c88da8585df5
    Former-commit-id: edd5b5944a0b38ccff3e19d61f0e12ef3a1a3e3e

--- tests/test_methods.php
+++ tests/test_methods.php
@@ -11,8 +11,8 @@ include_once( 'methods.php' );
 //$pat = repeatPatToDays( $pat, '2017-04-11' );
 //var_dump( $pat );
 //print( "\nTest 2 </br> \n" );
-$pat = constructRepeatPattern( "Tue Wed", "", "2" );
-echo( $pat );
-print_r( repeatPatToDays( $pat, '2017-04-10' ) );
+$pat = constructRepeatPattern( "Sun,Wed", "first", "2" );
+print_r( $pat );
+print_r( repeatPatToDays( $pat, dbDate('today') ) );
 
 ?>

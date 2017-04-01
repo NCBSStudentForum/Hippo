132e0a78e5acb93246c2b3b4c51d4c7c4c78085b
Dilawar Singh <dilawars@ncbs.res.in>
2017-04-01 21:07:37 +0530

    good to go.
    Former-commit-id: ccc8302cdb22f09f561e08a7af55c85c41172de6
    Former-commit-id: 1ea0f28285fca4e49100d59b638d9aacb2bdea6c

---
+++ user_tolet.php
@@ -0,0 +1,68 @@
+<?php 
+
+include_once( "header.php" );
+include_once( "methods.php" );
+include_once( "database.php" );
+include_once 'tohtml.php';
+include_once 'check_access_permissions.php';
+
+mustHaveAnyOfTheseRoles( array( 'USER' ) );
+
+echo userHTML( );
+$user = $_SESSION[ 'user' ];
+
+
+// All alerts.
+$allAlerts = getTableEntries( 'alerts' );
+
+$count = array( );
+foreach( $allAlerts as $alert )
+    $count[ $alert[ 'value' ] ] = 1 + __get__( $count, $alert['value'], 0 );
+
+echo '<strong>Total alerts for <tt>TO-LET</tt> </strong>';
+echo ' <table border="1"> <tr> ';
+foreach( $count as $val => $num )
+    echo " <td> <tt>$val</tt>  $num</td> ";
+echo '</tr> </table>';
+
+$apartmentTypes = array( 'SHARE', '1BHK', '2BHK', '3BHK', 'STUDIO', 'PALACE' );
+$apartmentSelect = arrayToSelectList( 'value', $apartmentTypes );
+
+
+// Create alerts.
+echo ' <form action="user_tolet_action.php" method="post" > ';
+echo '<table> <tr> ';
+echo '<td> Add new alert: ' . $apartmentSelect . '</td>';
+echo ' <input type="hidden" name="login" value="' . $user . '" />';
+echo ' <input type="hidden" name="on_table" value="apartments" />';
+echo ' <input type="hidden" name="on_field" value="type" />';
+echo '<td> <button name="response" value="New Alert">Add new alert </button> </td>';
+echo ' </tr></table>';
+echo '</form>';
+
+$where = "login='$user' AND on_table='apartments' AND  on_field='type'";
+$myAlerts = getTableEntries( 'alerts', 'login', $where );
+
+if( count( $myAlerts ) > 0 )
+{
+    echo " <h2>My alerts</h2> ";
+    echo ' <table border="1"> ';
+    echo '<tr>';
+    foreach( $myAlerts as $alert )
+    {
+        echo ' <form action="user_tolet_action.php" method="post" > ';
+        echo '<td> ';
+        echo $alert[ 'value' ];
+        echo '<button name="response" 
+            title="Delete this alert" 
+            value="Delete">'. $symbDelete .'</button>';
+        echo '</td>';
+        echo '</form>';
+    }
+    echo '</tr></table>';
+}
+
+
+echo goBackToPageLink( "user.php", "Go back" );
+
+?>

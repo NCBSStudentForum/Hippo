46438da11ccf5f7cb7707c05e135416b0c59f1df
Dilawar Singh <dilawars@ncbs.res.in>
2017-05-23 14:23:42 +0530

    TIme to add the image.
    Former-commit-id: 29cde4d09ce906ea268a0663fc815ee7f70ffd6d
    Former-commit-id: 4f5e248665b219ca2285f3d2e79973f9ab8556d0

--- map.php
+++ map.php
@@ -1,3 +1,10 @@
+<script type="text/javascript">
+function showLabel( msg )
+{
+    document.getElementById("location_info").innertHtml = msg;
+}
+</script>
+
 <?php
 
 include_once "header.php";
@@ -6,17 +13,54 @@ include_once 'methods.php';
 
 $imageUrl = __DIR__ . "/data/ncbs_map_route_map_to_lecture_halls.jpeg";
 
-echo "<h1> Location of major lecture halls in NCBS </h1>";
+echo "<h1> NCSB Map</h1>";
 
 if( file_exists( $imageUrl ) )
 {
-    echo displayImage( $imageUrl, $width = "800px" );
+    echo  "Selected location <p id=\"location_info\"></p>";
+    echo displayImage( $imageUrl, $height = "auto", $width = "1000px", $usemap = 'ncbsmap' );
+    echo '
+    <map name="ncbsmap">
+      <area shape="rect" coords="0,0,500,500" 
+        onmouseover="showLabel( \'This is map\' )" />
+    </map>
+    ';
 }
 else
 {
     echo printWarning( "No map is found." );
 }
 
+echo closePage( );
+
 ?>
 
+<!--
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css" />
+<script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css"
+  integrity="sha512-07I2e+7D8p6he1SIM+1twR5TIrhUQn9+I6yjqD53JQjFiMf8EtC93ty0/5vJTZGF8aAocvHYNEDJajGdNx1IsQ=="
+  crossorigin=""/>
+<script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"
+  integrity="sha512-A7vV8IFfih/D732iSSKi20u/ooOfj/AGehOKq0f4vLT1Zr2Y+RX7C+w8A1gaSasGtRUZpF/NZgzSAu4/Gc41Lg=="
+  crossorigin=""></script>
 
+<h2> Testing area </h2>
+
+<div id="ncbsmap"></div>
+
+<script type="text/javascript" charset="utf-8">
+    
+var map = L.map('ncbsmap').setView([51.505, -0.09], 13);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(ncbsmap)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+</script>
+
+-->

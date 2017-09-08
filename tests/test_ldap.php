7f1fe798cc1518833136616b2df1c6970408a99d
Dilawar Singh <dilawars@ncbs.res.in>
2017-09-08 17:49:57 +0530

    Synchronize the google calendar using python script.
    Former-commit-id: c88df6d1ba0f477e6f86fce1b2805478e09b728b
    Former-commit-id: 1766bc3d291040b09fc91b5bb93a76f910deb3a1

--- tests/test_ldap.php
+++ tests/test_ldap.php
@@ -9,3 +9,4 @@ var_dump( getUserInfoFromLdap( 'ashok' ) );
 var_dump( getUserInfoFromLdap( 'colinj' ) );
 var_dump( getUserInfoFromLdap( 'rashi' ) );
 var_dump( getUserInfoFromLdap( 'jayaprakashp' ) );
+var_dump( getUserInfoFromLdap( 'hrishikeshn' ) );

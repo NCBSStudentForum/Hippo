b08a2111fe858ee8b87fc327dcd203488e4bd652
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-09 16:32:24 +0530

    Hippo is decorated.

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 <?php
-
+$calURL = calendarURL( );
 echo "<footer>
+    <a href=\"$calURL\">NCBS Google Calendar</a>
     <div class=\"bottom\">
     Above image is radomly chosen from 
         <a href=\"https://intranet.ncbs.res.in/photography\" target=\"_blank\">
         NCBS Photography club web-page</a>.
-     <br> <br>
-    <a target=\"_blank\" 
-        href=\"https://github.com/dilawar/Hippo\" target='_blank'
+     <br> 
+    <a target=\"_blank\" href=\"https://github.com/dilawar/Hippo\" target='_blank'
         >GNU-GPL (v3)</a>
     (c) Dilawar Singh 2016-17
     </div>

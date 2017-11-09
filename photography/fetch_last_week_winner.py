a3ccc5777e586947f0c01f49f60dc08d184ccc8e
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-09 13:05:26 +0530

    Works after setting proxy variables.

--- photography/fetch_last_week_winner.py
+++ photography/fetch_last_week_winner.py
@@ -1,37 +1,30 @@
 #!/usr/bin/env python
 
+import os
 import re
 import mechanize
 import mimetypes
 import urllib2
 import random
 
+os.environ[ 'http_proxy' ] = 'http://proxy.ncbs.res.in:3128'
+os.environ[ 'https_proxy' ] = 'http://proxy.ncbs.res.in:3128'
+
 br_ = mechanize.Browser()
-base_url_ = 'http://intranet.ncbs.res.in/photography'
+base_url_ = 'https://intranet.ncbs.res.in/photography'
+
+
+def log( msg ):
+    return 
+    with open( '/tmp/a.txt', 'a' ) as f:
+        f.write( msg + '\n' )
 
 def is_url_image(url):    
     mimetype,encoding = mimetypes.guess_type(url)
     return (mimetype and mimetype.startswith('image'))
 
-def check_url(url):
-    """Returns True if the url returns a response code between 200-300,
-       otherwise return False.
-    """
-    try:
-        headers={
-            "Range": "bytes=0-10",
-            "User-Agent": "MyTestAgent",
-            "Accept":"*/*"
-        }
-
-        req = urllib2.Request(url, headers=headers)
-        response = urllib2.urlopen(req)
-        return response.code in range(200, 209)
-    except Exception, ex:
-        return False
-
 def is_image_and_ready(url):
-    return is_url_image(url) and check_url(url)
+    return is_url_image(url) 
 
 def print_broser( ):
     global br_
@@ -39,16 +32,23 @@ def print_broser( ):
 
 def main( ):
     global br_, base_url_
-    res = br_.open( base_url_ )
+    log( 'trying url open' )
+    try:
+        res = br_.open( base_url_ )
+    except Exception as e:
+        log( 'failed to open %s' % e )
     urls = [ ]
     for l in br_.links( ):
         url = l.url 
         if is_image_and_ready( url ):
             urls.append(url)
+            log( url )
     print( random.choice( urls ) )
 
 
 if __name__ == '__main__':
+    log( 'running' )
     main()
+    log( 'Finished' )
 
 

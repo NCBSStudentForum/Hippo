f27fbd2de0d4c7217c068c6aa0a974970d5d8884
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-09 16:28:14 +0530

    Added.
    Former-commit-id: dac2d7ed0e6b7d213e9863ca963f806f2d54fb60
    Former-commit-id: a12dd0e0bec0bee5ff996363afe0a5bfdc7cbcd1

--- footer.php
+++ footer.php
@@ -2,9 +2,12 @@
 
 echo "<footer>
     <div class=\"bottom\">
-    Above image is radomly chosen from NCBS Photography club web-page.<br> <br>
+    Above image is radomly chosen from 
+        <a href=\"https://intranet.ncbs.res.in/photography\" target=\"_blank\">
+        NCBS Photography club web-page</a>.
+     <br> <br>
     <a target=\"_blank\" 
-        href=\"https://github.com/NCBSStudentForum/ncbs-hippo\" target='_blank'
+        href=\"https://github.com/dilawar/Hippo\" target='_blank'
         >GNU-GPL (v3)</a>
     (c) Dilawar Singh 2016-17
     </div>

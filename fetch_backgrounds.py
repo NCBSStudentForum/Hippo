b0e69f39d46c68e950b5072dfab8d85ddb39ddb8
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-10 14:09:42 +0530

    Figures with caption.
    Former-commit-id: a3477f9728aee42458186330d72d6ad6d8abf18c
    Former-commit-id: 9f85778a963b8b48d518095c38e69744eb892534

--- fetch_backgrounds.py
+++ fetch_backgrounds.py
@@ -2,20 +2,24 @@
 
 import os
 import re
-import mechanize
 import mimetypes
 import urllib2
 import random
+
 import PIL.Image
+import PIL.ImageChops
+from PIL import ImageDraw
+from PIL import ImageFont
+
 import cStringIO
+import lxml.html
 
 os.environ[ 'http_proxy' ] = 'http://proxy.ncbs.res.in:3128'
 os.environ[ 'https_proxy' ] = 'http://proxy.ncbs.res.in:3128'
 
-br_ = mechanize.Browser()
 base_url_ = 'https://intranet.ncbs.res.in/photography'
 
-background_dir_ = '_backgrounds'
+background_dir_ = './data/_backgrounds'
 if not os.path.exists( background_dir_ ):
     os.makedirs( background_dir_ )
 
@@ -31,39 +35,89 @@ def is_url_image(url):
 def is_image_and_ready(url):
     return is_url_image(url) 
 
-def print_broser( ):
-    global br_
+def writeOnImage( img, caption ):
+    draw = ImageDraw.Draw(img)
+    # font = ImageFont.truetype(<font-file>, <font-size>)
+    font = ImageFont.truetype("./data/OpenSans-Regular.ttf", 12 )
+    fontCaption = ImageFont.truetype("./data/OpenSans-Regular.ttf", 20 )
+    draw.text((10, img.size[1] - 50)
+            , caption[0:80]
+            , (255,255,255)
+            , font=fontCaption
+            )
+    draw.text((10, img.size[1] - 15)
+            , '(c) NCBS Photography Club'
+            , (255,255,255)
+            , font=font
+            )
+    return img 
+
+def crop_surrounding_whitespace(image):
+    """Remove surrounding empty space around an image.
+
+    This implemenation assumes that the surrounding space has the same colour
+    as the top leftmost pixel.
+
+    :param image: PIL image
+    :rtype: PIL image
+    """
+    bg = PIL.Image.new(image.mode, image.size, image.getpixel((0, 0)))
+    diff = PIL.ImageChops.difference(image, bg)
+    bbox = diff.getbbox()
+    if not bbox:
+        return image
+    return image.crop(bbox)
 
 def main( ):
-    global br_, base_url_
-    log( 'trying url open' )
+    global base_url_
+    html = None
     try:
-        res = br_.open( base_url_ )
+        html = urllib2.urlopen( base_url_ ).read( )
+        assert html
     except Exception as e:
         log( 'failed to open %s' % e )
-    urls = [ ]
-    for l in br_.links( ):
-        url = l.url 
+        return 1
+
+    doc = lxml.html.fromstring( html )
+    tables = doc.xpath( '//table' ) 
+
+    images = [ ]
+    for table in tables:
+        trs = table.xpath( './/tr' )
+        for tr in trs:
+            image = {} 
+            tds = tr.xpath( './/td' )
+            for td in tds:
+                links = td.xpath( './/a' )
+                if links:
+                    for l in links:
+                        if l.text:
+                            image[ 'caption' ] = l.text
+                        if is_url_image( l.attrib[ 'href' ] ):
+                            image[ 'url' ] = l.attrib[ 'href' ]
+            images.append( image )
+
+    for im in images:
+        if not im:
+            continue
+        url = im[ 'url' ]
+        caption = im.get( 'caption', '' )
         if is_image_and_ready( url ):
-            urls.append(url)
             outfile = os.path.basename( url )
             outpath = os.path.join( background_dir_, outfile + '.jpg' )
             if not os.path.exists( outpath ):
                 try:
                     img = cStringIO.StringIO( urllib2.urlopen( url ).read( ) )
                     img = PIL.Image.open( img )
+                    img = crop_surrounding_whitespace( img )
                     width = 800
                     height = int((float(img.size[1])*width/float(img.size[0])))
                     img = img.resize( (width,height), PIL.Image.ANTIALIAS )
+                    writeOnImage( img, caption )
                     img.save( outpath )
                 except Exception as e:
                     print( e )
                     pass
-
-            log( url )
-
-    # This we write to stdout.
-    print( random.choice( urls ) )
 
 
 if __name__ == '__main__':

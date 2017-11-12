27df2ebd4cb2f1e3bedd64da0332bfc58e337b76
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-12 11:57:20 +0530

    Code which may be used more than one time should be in snippet directory. Add PI or SPECIALIZATION code in one script.

--- user_info.php
+++ user_info.php
@@ -3,6 +3,7 @@
 include_once 'header.php';
 include_once 'tohtml.php' ;
 include_once './check_access_permissions.php';
+include_once './snippets/pi_specialization.php';
 
 echo userHTML( );
 
@@ -13,38 +14,6 @@ if( ! isAuthenticated( ) )
     exit;
 }
 
-$specialization = array( );
-foreach( getAllSpecialization( ) as $spec )
-    $specialization[$spec[ 'specialization' ] ] = 1;
-$specialization = array_keys( $specialization );
-
-$info = getUserInfo( $_SESSION['user'] );
-
-// Collect all faculty
-$faculty = getFaculty( );
-$facultyByEmail = array( );
-foreach( $faculty as $fac )
-    $facultyByEmail[ $fac[ 'email' ] ] = $fac;
-
-$facEmails = array_keys( $facultyByEmail );
-
-?>
-
-<script type="text/javascript" charset="utf-8">
-// Autocomplete pi.
-$( function() {
-    // These emails must not be key value array.
-    var emails = <?php echo json_encode( $facEmails ); ?>;
-    var specialization = <?php echo json_encode( $specialization ); ?>;
-    $( "#logins_pi_or_host" ).autocomplete( { source : emails }); 
-    $( "#logins_pi_or_host" ).attr( "placeholder", "type email of your supervisor" );
-    $( "#logins_specialization" ).autocomplete( { source : specialization }); 
-    $( "#logins_specialization" ).attr( "placeholder", "Your specialization" );
-});
-</script>
-
-
-<?php
 $conf = getConf( );
 $picPath = $conf['data']['user_imagedir'] . '/' . $_SESSION['user'] . '.jpg';
 

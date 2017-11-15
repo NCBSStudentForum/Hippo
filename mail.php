55dcf80d2f648ad28528dbf3a6de071f23dff163
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-15 14:42:59 +0530

    Send HTML emails by default.

--- mail.php
+++ mail.php
@@ -105,6 +105,82 @@ function mailFooter( )
         ";
 }
 
+function sendHTMLEmail( $msg, $sub, $to, $cclist = '', $attachment = null )
+{
+    global $maildir;
+    $conf = getConf( );
+    if( ! is_string( $msg ) )
+    {
+        error_log( "Email msg is not in string format" );
+        echo printInfo( 'Email msg not in string format' );
+        return;
+    }
+    printInfo( "Trying to send email to $to, $cclist with subject $sub" );
+    if( strlen( trim( $msg ) ) < 1 )
+        return;
+
+    if( ! array_key_exists( 'send_emails', $conf['global' ] ) )
+    {
+        echo printInfo( "Email service has not been configured." );
+        error_log( "Mail service is not configured" );
+        return;
+    }
+
+    if( $conf['global']['send_emails' ] == false )
+    {
+        echo alertUser( "<br>Sending emails has been disabled in this installation" );
+        return;
+    }
+
+
+    // Check if this email has already been sent.
+    $archivefile = $maildir . '/' . md5($sub . $msg) . '.email';
+    if( file_exists( $archivefile ) )
+    {
+        echo printInfo( "This email has already been sent. Doing nothing" );
+        return;
+    }
+
+    printInfo( "... preparing email" );
+
+    $timestamp = date( 'r', strtotime( 'now' ) );
+
+    $msg .= mailFooter( );
+    $textMail = $msg; 
+
+    $msgfile = tempnam( '/tmp', 'hippo_msg' );
+    file_put_contents( $msgfile, $textMail );
+
+    $to =  implode( ' -t ', explode( ',', trim( $to ) ) );
+
+    // Use \" whenever possible. ' don't escape especial characters in bash.
+    $cmd= __DIR__ . "/sendmail.py -t $to -s \"$sub\" -i \"$msgfile\" -H";
+
+    if( $cclist )
+    {
+        $cclist =  implode( ' -c ', explode( ',', trim( $cclist ) ) );
+        $cmd .= "-c $cclist";
+    }
+
+    if( $attachment )
+    {
+        foreach( explode( ',', $attachment ) as $f )
+            $cmd .= " -a \"$f\" ";
+    }
+
+    $out = `$cmd`;
+
+    error_log( "<pre> $cmd </pre>" );
+    error_log( '... $out' );
+    error_log( "Saving the mail in archive" . $archivefile );
+
+    // generate md5 of email. And store it in archive.
+    file_put_contents( $archivefile, "SENT" );
+    unlink( $msgfile );
+    return true;
+
+}
+
 
 function sendPlainTextEmail($msg, $sub, $to, $cclist='', $attachment = null) 
 {

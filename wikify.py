6b717dd2ccbf79fc3c0959f55933366ebe9519ca
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-17 16:49:31 +0530

    Wikify the link.

--- wikify.py
+++ wikify.py
@@ -14,28 +14,58 @@ __status__           = "Development"
 import nltk
 try:
     from nltk.corpus import stopwords
+    from nltk.corpus import punkt
+    from nltk.corpus import averaged_perceptron_tagger
+    from nltk.corpus import words
+    from nltk.corpus import wordnet
 except Exception as e:
     nltk.download( 'stopwords' )
+    nltk.download( 'punkt' )
+    nltk.download( 'words' )
+    nltk.download( 'wordnet' )
+    nltk.download('averaged_perceptron_tagger')
+
 
 from nltk.corpus import stopwords
+from nltk.corpus import wordnet
 import sys
 import os
 from wikiapi import WikiApi
+import urllib2
 
 wiki_ = WikiApi( )
-common_ = set( stopwords.words( 'english' ) )
+common_ = set( nltk.corpus.words.words() )
+
+isNoun = lambda x: x[:2] == 'NN'
+
+def url_exists( url ):
+    ret = urllib2.urlopen( url )
+    if ret.code == 200:
+        return True
+    return False
+
+def wiki_link( query ):
+    wikiLink = 'https://en.wikipedia.org/wiki/%s' % query
+    if url_exists( wikiLink ):
+        return wikiLink
+    return query
+
+def wikify( text ):
+    tokens = nltk.word_tokenize( text )
+    nouns = [ word for (word,pos) in nltk.pos_tag(tokens) if isNoun(pos) ]
+    for n in nouns:
+        nn = wordnet.morphy( n )
+        if nn is not None and nn not in common_:
+            link = wiki_link( nn )
+            print(n, link )
 
 def main( ):
-    query = sys.argv[1]
-    print( 'Searching for %s' % query )
-    if query in common_:
-        print( 'Very common word' )
-        return ''
+    text = ''
+    with open( sys.argv[1], 'r' ) as f:
+        text = f.read( )
 
-    res = wiki_.find( query )
-    if len(res) > 1:
-        art = wiki_.get_article( res[0] )
-        print(art.url)
+    text = wikify( text )
+
 
 
 if __name__ == '__main__':

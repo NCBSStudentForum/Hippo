7cde337e84d7aa5c345179504e085cdf79a4e296
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-18 09:05:43 +0530

    Source file appropriately.
    Former-commit-id: 5c5a8300bfad221a9bda18b30928ee4c86bc1ee4
    Former-commit-id: 862209b005d6938f6608610a2d9a4989565f23f6

--- hippo_cron.sh
+++ hippo_cron.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -x
-
+set -e
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [ -f /opt/rh/rh-php56/ ]; then
+if [ -d /opt/rh/rh-php56/ ]; then
     source /opt/rh/rh-php56/enable 
+fi
+if [ -f /var/log/hippo.log ]; then
+    echo "Runnung $0: " >> /var/log/hippo.log
 fi
 php -f ${SCRIPT_DIR}/hippo_cron.php

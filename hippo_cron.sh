f601ad0107d3249c1cdb4d649f52c35996bd8cdc
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-18 18:02:30 +0530

    Everything is now auto update.

--- hippo_cron.sh
+++ hippo_cron.sh
@@ -22,8 +22,11 @@ SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 php -f ${SCRIPT_DIR}/hippo_cron.php
 # Now update the calendar. every six hours.
 HOUR=`date +%H`
+MINS=`date +%M`
 n=$((HOUR%6))
 if [ $n -eq 0 ]; then
-    log_msg "Updating google calendar."
-    php -f ${SCRIPT_DIR}/synchronize_calendar.php
+    if [[ $MINS -gt 0 && $MINS -lt 12 ]]; then
+        log_msg "Updating google calendar."
+        php -f ${SCRIPT_DIR}/synchronize_calendar.php
+    fi
 fi

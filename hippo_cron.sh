c495b794753ad72a22d06e3a6dea8711cda00ae1
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-18 09:22:29 +0530

    If cron fails notify admin.
    Former-commit-id: fb2fc27deb414e72fb5572a3381989d5d6969914
    Former-commit-id: fc934b67f32cce8c190a84b1475e8b59ce141ed4

--- hippo_cron.sh
+++ hippo_cron.sh
@@ -9,3 +9,9 @@ if [ -f /var/log/hippo.log ]; then
     echo "Runnung $0: " >> /var/log/hippo.log
 fi
 php -f ${SCRIPT_DIR}/hippo_cron.php
+exit_status=$?
+if [ ! $exit_status -eq 0 ]
+    echo "Failed to run CRON job. Automatic notification will fail." > /tmp/__alert 
+    python ${SCRIPT_DIR}/sendmail.py -s "Failed to run cron job" \
+        -i /tmp/__altert -t "hippo@lists.ncbs.res.in"
+fi

65dcfba476fb7324390cee2ba65b23a43304d1ba
Dilawar Singh <dilawars@ncbs.res.in>
2017-12-26 10:50:24 +0530

    removed debug message.

--- user_jc_admin_submit.php
+++ user_jc_admin_submit.php
@@ -65,7 +65,6 @@ else if( $_POST['response'] == 'DO_NOTHING' )
 }
 else if( $_POST['response'] == 'delete' )
 {
-    var_dump( $_POST );
     echo printInfo( "Removing user from subscription list" );
     $_POST[ 'status' ] = 'UNSUBSCRIBED';
     $res = updateTable( 'jc_subscriptions', 'login,jc_id', 'status', $_POST );

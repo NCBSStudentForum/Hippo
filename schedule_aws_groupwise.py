a588c804c7d1dbb5a38b10aa7ddb15b604381e90
Dilawar Singh <dilawars@ncbs.res.in>
2018-01-12 09:59:02 +0530

    Now we can compute the year long schedule after frequency bug fix.

--- schedule_aws_groupwise.py
+++ schedule_aws_groupwise.py
@@ -338,7 +338,7 @@ def construct_flow_graph(  ):
             g_.add_edge( 'source', speaker, capacity = 1, weight = 0 )
 
     # Compute totalWeeks of schedule starting today.
-    totalWeeks = 35
+    totalWeeks = 50
     today = datetime.date.today()
     nextMonday = today + datetime.timedelta( days = -today.weekday(), weeks=1)
     slots = []

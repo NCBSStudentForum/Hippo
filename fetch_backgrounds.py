4b5f129bc51a8f0851c8d8a06fc88d14f612a96c
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-08 14:33:44 +0530

    If author name is not found, continue.

--- fetch_backgrounds.py
+++ fetch_backgrounds.py
@@ -159,7 +159,14 @@ def get_images_from_dropbox( ):
         caption = img[ 'title' ]
         fname = img_to_fname( img )
         outfile = os.path.join( background_dir_, fname )
-        if img['average_votes'] is None or float( img[ 'average_votes'] ) < 3.0:
+        if img['author'] in [ 'N/A', 'NA', None ]:
+            print( '[WARN] No author or hidden' )
+            continue
+
+        if img['average_votes'] is None:
+            continue
+
+        if float( img[ 'average_votes'] ) < 3.0:
             print( '[INFO] Not enough votes. Ignoring' )
             # Delete this if it was already there.
             if os.path.exists( outfile ):

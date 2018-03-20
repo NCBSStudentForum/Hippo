434d8a47014eca6ad38c93a9c05424ae5dace1f2
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-20 10:36:07 +0530

    Needs to test proxy on hippo.

--- ldap.php
+++ ldap.php
@@ -39,9 +39,7 @@ function getUserInfoFromLdap( $query, $ldap_ip="ldap.ncbs.res.in" )
     // }
 
     $ds = @ldap_connect($ldap_ip, $port );
-
-    ldap_set_option( $ds, LDAP_OPT_TIMELIMIT, 1 );
-
+    @ldap_set_option( $ds, LDAP_OPT_TIMELIMIT, 1 );
     $r = @ldap_bind($ds);
 
     if( ! $r )
@@ -54,7 +52,8 @@ function getUserInfoFromLdap( $query, $ldap_ip="ldap.ncbs.res.in" )
     $sr = @ldap_search($ds, $base_dn, "uid=$login");
     $info = @ldap_get_entries($ds, $sr);
 
-    $result = array();
+    var_dump( $info );
+
     for( $s=0; $s < $info['count']; $s++)
     {
         $i = $info[$s];
@@ -73,8 +72,8 @@ function getUserInfoFromLdap( $query, $ldap_ip="ldap.ncbs.res.in" )
         $profileidentification = $profileId[0];
         $title = $i[ 'profilecontracttype'][0];
         $designation = $i[ 'profiledesignation'][0];
-
         $active = $i[ 'profileactive' ][0];
+
         $result[ ] =  array( "fname" => $i['profilefirstname'][0]
                 , "first_name" => $i['profilefirstname'][0]
                 , "lname" => $i['profilelastname'][0]
@@ -117,11 +116,8 @@ function authenticateUsingLDAP( $user, $pass )
     $ports = array( "ncbs" => 389, "instem" => 18288, "ccamp" => 19554 );
     foreach(  $ports as $dc => $port )
     {
-        $res = @ldap_connect( "ldap.ncbs.res.in", $port ) or
-            die( "Could not connect to ldap on port $port" );
-
+        $res = @ldap_connect( "ldap.ncbs.res.in", $port );
         $ldapQuery = "uid=$user,ou=People,dc=$dc,dc=res,dc=in";
-
         if( $res )
         {
             $bind = @ldap_bind( $res, $ldapQuery, $pass );

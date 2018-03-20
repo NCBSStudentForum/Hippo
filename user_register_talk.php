800dcb79edd42e884ffdd43646bde21abeceb8a2
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-20 11:15:03 +0530

    Don't query ldap.

--- user_register_talk.php
+++ user_register_talk.php
@@ -8,12 +8,14 @@ include_once 'check_access_permissions.php';
 
 mustHaveAnyOfTheseRoles( array( 'USER' ) );
 
+
 echo userHTML( );
 
 // Javascript.
 $faculty = getFaculty( );
 $speakers = getTableEntries( 'speakers' );
 $logins = getTableEntries( 'logins' );
+
 
 $speakersMap = array( );
 foreach( $speakers as $visitor )
@@ -102,6 +104,8 @@ echo '<table><tr>';
 echo '<td class="db_table_fieldname">Speaker picture</td><td>';
 echo '<input type="file" name="picture" id="picture" value="" />';
 echo '</td></tr></table>';
+
+exit( );
 
 echo dbTableToHTMLTable( 'speakers', $speaker
     , 'honorific,email,homepage,first_name:required,middle_name,last_name'

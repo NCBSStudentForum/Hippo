b0644404ae534fc313f38fba68e7ae5b4446f2c3
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-20 11:03:55 +0530

    Proxy error in user.php page.

--- user_register_talk.php
+++ user_register_talk.php
@@ -1,13 +1,12 @@
 <?php
 
 include_once 'header.php';
-
-include_once 'check_access_permissions.php';
-mustHaveAnyOfTheseRoles( array( 'USER' ) );
-
 include_once 'database.php';
 include_once 'tohtml.php';
 include_once 'methods.php';
+include_once 'check_access_permissions.php';
+
+mustHaveAnyOfTheseRoles( array( 'USER' ) );
 
 echo userHTML( );
 

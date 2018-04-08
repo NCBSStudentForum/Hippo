b4a2139fcc19a8e569a3c50f8dcdc273da140c6c
Dilawar Singh <dilawars@ncbs.res.in>
2018-04-08 11:15:35 +0530

    test script for pyramid.

---
+++ PY/hippo.py
@@ -0,0 +1,22 @@
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2017-, Dilawar Singh"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+from wsgiref.simple_server import make_server 
+from pyramid.config import Configurator 
+from pyramid.response import Response
+
+
+def hippo(request):
+    return Response('Hello %(name)s!' % request.matchdict)
+
+if __name__ == '__main__':
+    with Configurator() as config:
+        config.add_route('hippo', '/hippo/{name}')
+        config.add_view(hippo, route_name='hippo')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 8081, app)
+    server.serve_forever()

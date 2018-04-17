4d55296e655068b0ccdff5bf010764813ec4ce54
Dilawar Singh <dilawars@ncbs.res.in>
2018-04-17 15:44:22 +0530

    Page to update AWS entries.

--- admin_acad.php
+++ admin_acad.php
@@ -132,11 +132,14 @@ if( isset( $_POST[ 'response' ] ))
         echo '<br>';
 
         /* This forms remain on this page only */
-        echo '<form method="post" action="">
+        echo '<form method="post" action="admin_acad_edit_aws.php">
             <input type="hidden" name="speaker" value="' . $speaker . '">
             <input type="hidden" name="date" value="' . $date . '" >
-            <button onclick="AreYouSure(this)"
-                style=\"float:right\" name="response" >'. $symbDelete . '</button>
+            <input type="hidden" name="id" value="' . $aws['id'] . '" >
+                <button onclick="AreYouSure(this)" style=\"float:right\" name="response" value="delete" >
+                    <i class="fa fa-trash"></i>
+                </button> <button style=\"float:right\" name="response" value="edit" >
+                    <i class="fa fa-pencil"></i> </button>
             </form>
             ';
     }

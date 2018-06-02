d03bfda21d9a794584d56ac7d0c8ce635884d2a7
Dilawar Singh <dilawars@ncbs.res.in>
2018-06-02 11:18:44 +0530

    python3 compatible script.

--- html2other.py
+++ html2other.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 __author__           = "Dilawar Singh"
 __copyright__        = "Copyright 2016, Dilawar Singh"
@@ -13,15 +14,19 @@ import sys
 import os
 import subprocess
 import re
-import html2text
+import six
 import string
 import tempfile
 import base64
 from logger import _logger
+import locale
+
+# Set locale to utf-8 otherwise calling this script from apache will have
+# decoding issues.
+locale.setlocale( locale.LC_ALL, "en_IN.UTF-8")
+os.environ[ "PYTHONENCODING"] = "utf-8"
 
 pandoc_ = True
-
-
 
 def _cmd( cmd ):
     output = subprocess.check_output( cmd.split( ), shell = False )
@@ -32,22 +37,24 @@ def fix( msg ):
 
 def tomd( htmlfile ):
     # This function is also used by sendemail.py file.
-    with open( htmlfile ) as f:
-        msg = f.read( )
+    with open( htmlfile, 'r' ) as f:
+        msg = f.read()
 
     msg = fix( msg )
     # remove <div class="strip_from_md"> </div>
     pat = re.compile( r'\<div\s+class\s*\=\s*"strip_from_md"\s*\>.+?\</div\>', re.DOTALL )
     for s in pat.findall( msg ):
         msg = msg.replace( s, '' )
-    msg = msg.replace( '</div>', '' )
+    msg = msg.replace( u'</div>', '' )
     msg = re.sub( r'\<div\s+.+?\>', '', msg )
 
     # Write back to original file.
     htmlfile = tempfile.mktemp( prefix = 'hippo', suffix='.html' )
     txtfile = '%s.txt' % htmlfile
+
     with open( htmlfile, 'w' ) as f:
-        f.write( msg )
+        f.write(msg)
+
     _cmd( 'pandoc -t plain -o %s %s' % (txtfile, htmlfile) )
     return msg, txtfile.strip()
 
@@ -57,36 +64,35 @@ def fixInlineImage( msg ):
 
     Surround each image with \includewrapfig environment.
     """
-
     # Sometime we loose = in the end.
-    pat = re.compile( r'\{data:image/(.+?);base64,(.+?\=?\})', re.DOTALL )
+    pat = re.compile( r'data\:image\/(?P<fmt>.+?);base64,(?P<data>.+?=)', re.DOTALL | re.I )
     for m in pat.finditer( msg ):
-        outfmt = m.group( 1 )
-        data = m.group( 2 )
-        fp = tempfile.NamedTemporaryFile( delete = False, suffix='.'+outfmt )
-        fp.write( data )
+        outfmt = m.group( 'fmt' )
+        data = m.group( 'data' )
+        fp = tempfile.NamedTemporaryFile( mode='w+b', delete = False, suffix='.'+outfmt )
+        fp.write( base64.decodebytes(data.encode()) )
         fp.close( )
         # Replace the inline image with file name.
-        msg = msg.replace( m.group(0), "{%s}" % fp.name )
+        msg = msg.replace( m.group(0), "%s" % fp.name )
 
     # And wrap all includegraphics around by wrapfig
     msg = re.sub( r'(\\includegraphics.+?width\=(.+?)([\],]).+?})'
             , r'\n\\begin{wrapfigure}{R}{\2}\n \1 \n \\end{wrapfigure}'
             , msg, flags = re.DOTALL
             )
-
     return msg
 
 def toTex( infile ):
     outfile = tempfile.mktemp(  prefix = 'hippo', suffix = 'tex'  )
-    msg = _cmd( 'pandoc -f html -t latex -o %s %s' % (outfile, infile ))
-    if os.path.isfile( outfile ):
-        with open( outfile, 'rb' ) as f:
-            msg = fixInlineImage( f.read( ).decode('utf-8') )
-    else:
-        with open( outfile, 'w' ) as f:
-            f.write( "Could not covert to TeX" );
-    return outfile
+    _cmd( 'pandoc -f html -t latex -o %s %s' % (outfile, infile ))
+
+    with open( outfile, 'r' ) as f:
+        tex = f.read()
+    tex = fixInlineImage( tex )
+    with open( outfile, 'w' ) as f:
+        f.write( tex )
+
+    return outfile.strip()
 
 def htmlfile2md( filename ):
     md, mdfile = tomd( filename )
